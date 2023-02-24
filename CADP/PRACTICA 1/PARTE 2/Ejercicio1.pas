a434ad96e6928d8e4e895203567f86d7fb0e557d
Hernan <83614099+Hernan404@users.noreply.github.com>
2023-02-24 13:42:00 -0300

    Create Ejercicio1.pas

---
+++ CADP/PRACTICA 1/PARTE 2/Ejercicio1.pas
@@ -0,0 +1,26 @@
+program EJ1P2;
+
+var
+	num1:integer;
+	i: integer;
+	resultado, mayor:integer;
+
+begin
+	i:= 0;
+	num1:= 0;
+	resultado:=0;
+	mayor:= 0;
+	
+	for i:= 0 to 2 do begin
+		readln(num1);
+		if (num1 > 5) then begin
+			mayor:= mayor+1;
+		end;
+		resultado:= num1+resultado;
+		
+	end;
+	
+	writeln('la suma de numeros es ', resultado);
+	writeln('cantidad de numeros mayores: ', mayor);
+
+end.
