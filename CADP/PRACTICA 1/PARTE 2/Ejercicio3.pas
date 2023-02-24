8e69c287a23b39948dc3f8990af6a4fb7c8812d1
Hernan <83614099+Hernan404@users.noreply.github.com>
2023-02-24 16:22:01 -0300

    Create Ejercicio3.pas

---
+++ CADP/PRACTICA 1/PARTE 2/Ejercicio3.pas
@@ -0,0 +1,32 @@
+program EJ3P1P2;
+
+var
+	aprobado, con7, nota:integer;
+	nombre:string;
+	
+
+begin
+	aprobado:= 0;
+	con7:= 0; 
+	
+	
+repeat	
+	writeln('ingrese nombre');
+	readln(nombre);
+	writeln('ingrese nota');
+	readln(nota);
+	
+	if (nota >= 8) then
+		aprobado:= aprobado+1;
+	if (nota = 7) then
+		con7:= con7+1;
+		
+			
+		
+until nombre = 'zidane';		
+	
+writeln('cantidad de aprobados ', aprobado);
+writeln('cantidad con 7 ', con7);
+
+
+end.
