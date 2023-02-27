108e84a1ccbc0ecdfd4624e902a71933fc745699
Hernan <83614099+Hernan404@users.noreply.github.com>
2023-02-27 14:17:11 -0300

    Create Ejercicio5.pas

---
+++ CADP/PRACTICA 1/PARTE 2/Ejercicio5.pas
@@ -0,0 +1,30 @@
+Program EJ5P1P2;
+
+var
+
+	num,min,max,suma:integer;
+
+begin
+	max:= -9999;
+	min:= 9999;
+	suma:= 0;
+
+	repeat
+		readln(num);
+		
+		suma:= suma+num;
+		
+		if (num > max) then
+			max:= max+num;
+		if (num < min) then
+			min:= min+num;
+		
+	
+	until num = 100;
+
+	writeln('el maximo es ' max);
+	writeln('el minimo es ' min);
+	writeln('la suma total es ' suma);
+	
+
+end
