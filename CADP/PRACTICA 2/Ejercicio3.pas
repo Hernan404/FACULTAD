d7d9ed8e3d8e978c375cc8bf429a117982bdd678
Hernan <83614099+Hernan404@users.noreply.github.com>
2023-03-12 09:03:08 -0300

    Create Ejercicio3.pas

---
+++ CADP/PRACTICA 2/Ejercicio3.pas
@@ -0,0 +1,16 @@
+3. Dado el siguiente programa, indicar cuál es el error y su causa.
+program alcance3;
+var a: integer;
+procedure uno;
+var b: integer;
+begin
+b:= 2;
+writeln(b);
+end;
+begin
+a:= 1;
+uno;
+writeln(a, b);
+end.
+
+NO HAY VARIABLE GLOBAL 'B'
