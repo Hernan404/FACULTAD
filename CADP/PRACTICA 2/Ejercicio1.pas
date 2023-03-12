dce089460c42ae177b5bcc4ae5bcafae36a01593
Hernan <83614099+Hernan404@users.noreply.github.com>
2023-03-12 08:49:06 -0300

    Create Ejercicio1.pas

---
+++ CADP/PRACTICA 2/Ejercicio1.pas
@@ -0,0 +1,19 @@
+{1. Dado el siguiente programa, indicar qué imprime.}
+
+program alcance1;
+var a,b: integer;
+procedure uno;
+var b: integer;
+begin
+b := 3;
+writeln(b);
+end;
+begin
+a:= 1;
+b:= 2;
+uno;
+writeln(b, a);
+end.
+
+
+VA A LEER 1 Y 3 POR QUE SE SOBREESCRIBE
