2d68ca7d7c2372247e1c8cfd085f7798249b6348
Hernan <83614099+Hernan404@users.noreply.github.com>
2023-03-12 08:50:04 -0300

    Create Ejercicio2.pas

---
+++ CADP/PRACTICA 2/Ejercicio2.pas
@@ -0,0 +1,17 @@
+2. Dado el siguiente programa, indicar qué imprime.
+program alcance2;
+var a,b: integer;
+procedure uno;
+begin
+b := 3;
+writeln(b);
+end;
+begin
+a:= 1;
+b:= 2;
+uno;
+writeln(b, a);
+end.
+
+
+VA A LEER 2 Y 1 POR QUE NO HAY VARIABLE EN EL PROCEDIMIENTO
