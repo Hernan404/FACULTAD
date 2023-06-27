59bec46284b3bad60b7d54b86e6f608f8234c322
Hernan <83614099+Hernan404@users.noreply.github.com>
2023-06-27 09:19:09 -0300

    Update notas.pas

--- CADP/notas.pas
+++ CADP/notas.pas
@@ -1,3 +1,24 @@
+SACAR DIGITOS PARA SABER SI SON PARES 
+procedure subru (numerosF:longint;   var sumapar,cantimpar:integer );
+var
+    digitos:integer;
+    
+begin
+    while (numerosF <> 123456) do begin
+		
+        digitos:= numerosF mod 10;
+
+        if (digitos div 2 = 0) then 
+            sumapar:= sumapar + digitos
+        else
+            cantimpar:= cantimpar +1;
+            
+        numerosF:= numerosF DIV 10
+    end;
+end;
+--------------------------------------------------------
+
+
 
 VECTOR CON DIML 
 program EJ3P4;
