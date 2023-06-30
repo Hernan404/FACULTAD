37cf6910ad04c4ac0e0a1972528b8ac9509d48e2
Hernan <83614099+Hernan404@users.noreply.github.com>
2023-06-30 10:20:48 -0300

    Update Ejercicio1.pas

--- CADP/PRACTICA 7/Ejercicio1.pas
+++ CADP/PRACTICA 7/Ejercicio1.pas
@@ -1,184 +1,200 @@
-program EJ1P7; 
+
+program EJ1P7;
 type 
-	data = record 
-		DNI:integer;
-		nombre:string;
-		edad:integer;
-		codigo:string; 
-	end;
+    actores = record
+        DNI:integer;
+        apellidoYnombre:string;
+        edad:integer;
+        codigo:1..7;
+    end;
 
-	lista = ^nodo;
-	nodo = record 
-		D:data;
-		sig:lista;
-	end; 
+    delete = record 
+        DNI2:integer;
+    end;
 
-vector = array [1..7] of integer;
+    lista = ^nodo; 
+    nodo = record 
+        datos:actores;
+        sig:lista;
+    end;
 
-procedure leer (D:data);
+   { lista2 = ^nodo;
+    nodo = record 
+        datos:delete;
+        sig:lista2;
+    end;}
+
+    vector = array [1..7] of integer;
+
+procedure leer(var d:actores);
 begin
-	with D do begin 
-		writeln('dni');
-		readln(DNI);
-		writeln('nombre');
-		readln(nombre);
-		writeln('edad');
-		readln(edad);
-		writeln('codigo');
-		readln(codigo);
+	with d do begin 
+    writeln('DNI');
+    readln(DNI);
+    writeln('edad');
+    readln(edad);
+    writeln('codigo');
+    readln(codigo);
 	end;
 end;
 
-procedure armarnodo(L:lista; D:data);
+procedure inivector (var v:vector);
 var 
-	aux:lista;
+    i:integer;
 begin 
-	new(aux);
-	aux^.D:= D;
-	aux^.sig:= L;
-	L:= aux;
-end; 
-
-procedure loscodigos (var D:data; v:vector);
-begin 
-	with D do begin 
-	if (codigo = 'drama') then
-		v[1]:= v[1]+1;
-	if (codigo = 'romantico') then
-		v[2]:= v[2]+1;
-	if (codigo = 'accion') then
-		v[3]:= v[3]+1;
-	if (codigo = 'suspenso') then
-		v[4]:= v[4]+1;
-	if (codigo = 'terror') then
-		v[5]:= v[5]+1;
-end;
+    for i:=1 to 7 do begin 
+        v[i]:= 0;
+    end;
 end;
 
-procedure cargarLista (var L:lista; var D:data; var v:vector);
-begin 
-	repeat 
-		armarnodo(L,D);
-		leer(D);
-		loscodigos(D,v);
-	until (D.dni <> 3);
-end;
-
-procedure cargarV (v:vector); 
+procedure cargarVector(var v:vector; d:actores);
 var 
-	i:integer;
+    i:integer;
 begin 
-	for i:=1 to 5 do begin 
-		v[i]:= 0;
-	end;
+    for i:= 1 to 7 do begin 
+        v[i]:= d.codigo;
+    end;
 end;
-
-
-
-
-
-procedure par_o_impar (var D:data; var digito:integer; var impar,par:integer);
-begin 
-	digito:= D.DNI mod 10;
-	
-	if (digito mod 2 = 0) then 
-		par:= par +1
-	else 
-		impar := impar +1;
-end;
-
-
-
-procedure maximos (var D:data; var v:vector; var max1,max2:integer);
+procedure armarlista (var L:lista; d:actores);
 var 
-	i:integer;
+    aux:lista;
 begin 
-	for i:=1 to 5 do begin
-		if (v[i] > max1) then begin 
-			max1:= v[i];
-			max2:= max2;
-		end
-			else if v[i] > max2 then begin 
-				max2:= v[i];
-			end;
-	end;
+    new(aux);
+    aux^.datos:= d;
+    aux^.sig:= L;
+    L:= aux;
 end;
 
-function buscar (L:lista; DNIB:integer):boolean;
+procedure cargarLista (L:lista);
 var 
-	encontre:boolean;
+	d:actores;
 begin 
-	encontre:= false;
-	while(L <> nil) and (not encontre) do begin 
-		if(DNIB = L^.D.DNI) then 
-			encontre:=true
-		else begin
-			L:= L^.sig;
-			encontre := false;
-		end;
-	buscar:= encontre;
-	end;
-	
+    repeat 
+        leer(d);
+        armarlista(L,d);
+    until (d.DNI = 3355); 
 end;
 
-
-
-
-
-procedure eliminar (pri:lista; datos:data; DNIB:integer; var exito:boolean);
+procedure puntoA(var d:actores; var cantCumple:integer);
 var 
-	ant,act:lista;
-begin 
-	exito:= false;
-	act:= pri;
-	while (act <> nil) and (act^.D.DNI <> DNIB) do begin 
-		ant:= act;
-		act:= act^.sig
-	end;
-	if (act <> NIL) then begin 
-		exito:= true;
-		if (act = pri) then 
-			pri:= act^.sig;
-			writeln('el DNI fue eliminado');
-	end
-		else 
-			ant^.sig:= act^.sig;
-		dispose(act);
-end;
-
-
-procedure estructura (var D:data; var pri,L:Lista; DNIB:integer);
-var 
-	exito:boolean;
-begin 
-	writeln('ingrese DNI que quiere buscar y eliminar');
-	readln(DNIB);
-	buscar(L,DNIB);
-	eliminar(pri,D,DNIB,exito);
-end;
-
-
-
-var 
-	par,impar:integer;
-	max1,max2:integer;
-	D:data;
-	pri:lista;
-	L:lista;
-	v:vector;
-	DNIB:integer;
-	digito:integer;
+    par,impar,digitos:integer;
 begin
-	DNIB:=0;
-	max1:= 0;
-	max2:= 0;
-	par:= 0;
-	impar:= 0;
-	cargarLista(L,D,v);
-	leer(D);
-	par_o_impar(D,digito,impar,par);
-	writeln('punto A): ', par);
-	maximos(D,v,max1,max2);
-	writeln('punto B): ', max1 ,'y', max2);
-	estructura(D,pri,L,DNIB)
+    par:= 0;
+    impar:= 0;
+    digitos:= 0;
+
+    digitos:= d.DNI mod 10; 
+    if (digitos mod 2 = 0) then begin 
+        par:= par +1;
+        d.DNI:= d.DNI div 10;
+    end
+    else 
+        begin
+        impar:= impar +1;
+        d.DNI:= d.DNI div 10;
+    end;
+
+    if (par > impar) then 
+        cantCumple:= cantCumple +1;
+end;
+
+procedure puntoB (vc:vector; var max1,max2:integer; var cumpleB1,cumpleB2:integer);
+var 
+    i:integer;
+begin 
+	for i:=1 to 7 do begin 
+    if (vc[i] > max1) then begin 
+        max1:= max1;
+		max1:= vc[i];
+        cumpleB1:= i;
+    
+    end
+    else 
+        begin 
+            if (vc[i] > max2) then 
+                max2:= vc[i];
+                cumpleB2:= i;
+        end;
+    end;
+end;
+
+{procedure armarlista2 (var L2:lista2; b:delete);
+var 
+    aux:lista2;
+begin 
+    new(aux)
+    aux ^.delete:=b;
+    aux ^.sig:= L;
+    L:= sig;
+end;}
+
+procedure eliminar (var L:lista; var d:actores; b:delete; var exito:boolean);
+var 
+    act,ant:lista;
+begin 
+    exito:= false;
+    act:= L;
+    {recorro mientras no se termina la lista y no encuentre el elemento}
+    while (act <> nil) and (act^.datos.DNI <> b.DNI2) do begin 
+        ant:= act;
+        act:= act^.sig
+    end;
+
+
+    if (act <> nil) then begin 
+        exito:= true;
+        if (act = L) then 
+            L:= act^.sig
+        else 
+            ant^.sig:= act^.sig;
+        dispose(act);
+    end;
+end;
+
+procedure procesarDatos (L:lista);
+var 
+    cumpleB1:integer;
+    cumpleB2:integer;
+    cantCumple:integer;
+    max1,max2:integer;
+    tipo:1..7;
+    v:vector;
+    d:actores;
+begin 
+	max1:= -1;
+	max2:= -1;
+    cantCumple:= 0;
+    cumpleB1:= 0;
+    cumpleB2:= 0;
+
+    inivector(v);
+    while (L <> nil) do begin 
+		tipo:= L^.datos.codigo;
+		v[tipo]:= v[tipo] +1;
+        puntoA(d,cantCumple);
+        puntoB(v,max1,max2,cumpleB1,cumpleB2);
+        
+
+    end;
+    
+    writeln('cantidad de personas con DNI digitos pares: ', cantCumple);
+    writeln('los codigos de generos mas elegidos: ', cumpleB1 , cumpleB2);
+end;
+
+var 
+    d:actores;
+    L:lista;
+    b:delete;
+   
+    exito:boolean;
+begin 
+	L:= nil;
+    cargarLista(L);
+    procesarDatos(L);
+
+    writeln('ingrese DNI a elliminar');
+    readln(b.DNI2);
+        {armarlista2(L2);}
+        eliminar(L,d,b,exito);
+    
 end.
