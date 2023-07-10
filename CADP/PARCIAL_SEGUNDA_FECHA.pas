7a49e83288406550aed39bace948ca647a7d7d92
Hernan <83614099+Hernan404@users.noreply.github.com>
2023-07-10 11:58:37 -0300

    Create PARCIAL_SEGUNDA_FECHA.pas

---
+++ CADP/PARCIAL_SEGUNDA_FECHA.pas
@@ -0,0 +1,82 @@
+program segundaFechaTema1;
+type 
+
+
+    rangoGenero = 1..5
+    sesion = record 
+        titulo: string;
+        artista:string;
+        genero:rangoGenero;
+        visualizaciones:integer;
+    end;
+
+    lista = nodo 
+    nodo = record 
+        datos:sesion;
+        sig:lista;
+    end;
+
+    vector = array [rangoGenero] of integer;
+
+procedure inivector (var v:vector);
+var 
+    i:integer;
+begin 
+    for i:=1 to 5 do begin 
+        v[i]:= 0;
+    end;
+end;
+
+procedure armarnodo();
+procedure leer();
+procedure cargarLista();
+
+procedure maximos (v:vector; var cod1,cod2:rangoGenero);
+var 
+    max1,max2:integer;
+    i:rangoGenero;
+begin 
+    max1:= -1;
+    max2:= -1;
+        for i:=1 to 5 do begin 
+            if (v[i] > max1) then begin 
+                max2:= max1;
+                cod2:= cod1;
+                cod1:= i;
+                max1:= v[i];
+            end
+                else if (v[i] > max2) then begin  
+                    max2:= v[i];
+                    cod1:= i;
+                end;
+        end;
+end;
+procedure procesarLista(L);
+var 
+    max1,max2:integer;
+    titulo:string;
+begin 
+    while (L <> nil) do begin 
+        v[L.datos.genero]:= v[L.dato.genero] + L.datos.visualizaciones;
+        if (cumple(L.dato)) then 
+            cant:= cant +1;
+        L:= L.sig;
+    end;
+    writeln(cant);
+    end;
+end;
+
+var 
+    L:lista;
+    v:vector;
+begin 
+    L:= nil;
+    inivector(v);
+    cargarLista(L);
+    procesarLista(L);  
+    maximos(v,codmax1,codmax2);
+    writeln(codmax1,codmax2);
+
+    readln(titulo);
+    eliminar(L);
+end.
