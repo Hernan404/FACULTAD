9d1f757e32dce09ef1a6455f4cd03013dd9864c4
Hernan <83614099+Hernan404@users.noreply.github.com>
2023-07-11 15:02:06 -0300

    Update Ejercicio7.pas

--- CADP/PRACTICA 7/Ejercicio7.pas
+++ CADP/PRACTICA 7/Ejercicio7.pas
@@ -1,126 +1,159 @@
-program EJ5P7;
-
+program EJ7P7;
 type 
-    data = record 
-        codigo_viaje:integer;
-        codigo_camion:integer;
-        distanciaKM:real;
-        ciudad_destino:integer;
-        anioviaje:integer;
-        DNI:integer;
-    end;
-
-    datadis = record 
-        patente:string;
-        anio_fabri:integer;
-        capacidad:real;
+    rangonota = 1..24;
+    alumno = record 
+        numero:integer;
+        nombreYapellido:string;
+        mail:string;
+        anio_ingreso:integer;
+        anio_egreso:integer;
+        nota:rangonota;
     end;
 
     lista = ^nodo;
     nodo = record 
-        lista:data;
+        datos:alumno;
         sig:lista;
     end;
 
+    vector = array [1..24] of integer;
 
-
-procedure leer(d:data);
-begin 
-	with d do begin 
-    readln(codigo_viaje);
-    readln(codigo_camion);
-    readln(distanciaKM);
-    readln(ciudad_destino);
-    readln(anioviaje);
-    readln(DNI);
-	end;
-end;
-
-procedure armarnodo(var L:lista; d:data);
+procedure armarnodo (L:lista; a:alumno);
 var 
     aux:lista;
 begin 
-	new(aux);
-    aux^.data:= d;
+    new(aux);
+    aux^.datos:= a;
     aux^.sig:= L;
-    aux:= lista
+    L:= aux;
 end;
+procedure leer(var a:alumno);
 
-
-
-procedure masrecor (var max:real; var min:real; var d:data; var masrecorrido:real; menosrecorrido:real);
-begin 
-    if (d.distanciaKM > max) then begin  
-        max:=d.distanciaKM;
-        masrecorrido:= max;
-    end;
-    if (d.distanciaKM < min) then begin 
-        min:= d.distanciaKM;
-        menosrecorrido:= min;
-    end;
-end;
-
-procedure mayorcapacidad (var d:data; var b:datadis; var cantViajes:integer);
-begin 
-    if (b.capacidad > 30.5) and (d.anioviaje > 5) then begin 
-        cantViajes:= cantViajes + 1;
-    end;
-end;
-
-procedure digitos (var L:lista; d:data; var codigocumple:integer; cumple:boolean);
+procedure cargarlista (L:lista);
 var 
-    dig:integer;
+    a:alumno;
 begin 
-
-    while (d.DNI <> 0) and (cumple <> true) do begin 
-        dig:= d.DNI mod 10;
-        if (dig / 2 <> 0) then
-            cumple:= true;
-    end; 
-    codigocumple:= d.codigo_viaje;
-end;
-
-procedure cargarDatos (L:lista; b:datadis; d:data; max,min:real; cantViaje:integer; masrecorrido,menosrecorrido:real; cumple:boolean; codigocumple:integer);
-begin 
-    while (d.codigo_viaje <> -1) do begin 
-        leer(d);
-        armarnodo(L,d);
-        masrecor(max,min,d,masrecorrido,menosrecorrido);
-        mayorcapacidad(d,b,cantViaje);
-        digitos(L,d,codigocumple,cumple);
+    leer(a);
+    while (a.numero <> -1) do begin 
+        leer(a);
+        armarnodo(L,a);
     end;
+end;
+
+procedure inivector (var v:vector);
+var 
+    i:rangonota;
+begin 
+    for i:=1 to 24 do begin 
+        v[i]:= 0;
+    end;
+end;
+
+function soloimpares (a:alumno):boolean;
+var 
+    par,impar,digito:integer;
+begin 
+    par:= 0;
+    impar:= 0;
+
+    while (a.numero <> 0) do begin 
+        digito:= a.numero mod 10;
+        if ((digito mod 2) = 0) then begin 
+            par:= par +1;
+        end
+        else
+            impar:= impar +1;
+    end;
+        a.numero:= a.numero div 10;
+    
+
+    soloimpares:= (par = 0);
+	
+end;
+
+function cumpleB (a:alumno):boolean;
+begin 
+    cumpleB:= (a.anio_ingreso = 2012) and (soloimpares(a));
+end;
+
+procedure puntoC (var L,p1,p2:lista; var min1,min2:integer; var a:alumno);
+begin 
+    if ((L^.datos.anio_egreso - L^.datos.anio_ingreso) < min1) then begin 
+        min2:= min1;
+        p2:= p1;
+        min1:= (L^.datos.anio_egreso - L^.datos.anio_ingreso);
+        p1:= L;
+    end
+    else 
+        if ((L^.datos.anio_egreso - L^.datos.anio_ingreso) < min2) then begin 
+            min2:= (L^.datos.anio_egreso - L^.datos.anio_ingreso);
+            p2:= L;
+        end;
     
 end;
 
-var
-    cantViajes:integer;
-    max,min:real;
-    menosrecorrido:real;
-    masrecorrido:real;
-    d:data;
-    L:lista;
-    b:datadis;
-    cumple:boolean;
-    dnidigitos:integer;
-    codigocumple:integer;
-begin
-	codigocumple:= 0;
-	cumple:= false;
-    menosrecorrido:= 0;
-    masrecorrido:= 0;
-    cantViajes:= 0;
-    max:= -1;
-    min:= 99999;
-    
-    L:= nil;
-    
-    cargarDatos(L,b,d,max,min,cantViajes,masrecorrido,menosrecorrido,cumple,codigocumple);
-  
+procedure eliminar (var pri:lista; a:alumno; var exito:boolean);
+var 
+    ant,act:lista;
+    numero:integer;
+begin 
+	readln(numero);
+	
+    exito:= false;
+    act:= pri;
 
-    writeln('menos recorrido ', menosrecorrido , 'mas recorrido: ', masrecorrido);
-    writeln('cantidad de viajes: ', cantViajes);
-    if (cumple = true) then 
-        writeln('el dni contiene pares')
+    while (act <> nil) and (act^.datos.numero <> a.numero) do begin 
+        ant:= act;
+        act:= act^.sig;
+    end;
+
+    if (act <> nil) then begin 
+        pri:= act^.sig;
+    end
     else 
-        writeln('el dni no contiene pares');
+        ant^.sig:= act^.sig;
+    dispose(act);
+end;
+
+procedure procesarLista (L:lista);
+var 
+    v:vector;
+    a:alumno;
+    cantcumpleB:integer;
+    min1,min2,cumpleC1,cumpleC2:integer;
+    cantalumnos,promnotas:integer;
+    p1,p2:lista;
+    a:alumno;
+    exito:boolean;
+begin 
+	cantcumpleB:= 0;
+	promnotas:= 0;
+    min1:= 9999;
+    min2:= 9999;
+    cantalumnos:= 0;
+    inivector(v);
+
+    while (L <> nil) do begin 
+        promnotas:= promnotas + v[L^.datos.nota];
+
+        soloimpares(a);
+        if cumpleB(L^.datos) then begin 
+            cantcumpleB:= cantcumpleB +1;
+        end;
+        puntoC(L,p1,p2,min1,min2,a);
+
+    end;
+    writeln('punto A', promnotas/24);
+    writeln('punto b', cantcumpleB);
+    writeln('punto C', p1^.datos.nombreYapellido, ' y ', p1^.datos.mail,'y', p2^.datos.nombreYapellido, 'y', p2^.datos.mail);
+   
+    eliminar(L,a,exito);
+end;
+
+var 
+    L:lista;
+begin 
+    L:= nil;
+    cargarlista(L);
+    procesarLista(L);
 end.
