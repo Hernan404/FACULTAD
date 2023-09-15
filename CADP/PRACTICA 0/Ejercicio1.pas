f7e1343ea6acc4ef204415162570c8dd263c6c12
Hernan <83614099+Hernan404@users.noreply.github.com>
2023-09-15 12:06:25 -0300

    Update Ejercicio1.pas

--- CADP/PRACTICA 0/Ejercicio1.pas
+++ CADP/PRACTICA 0/Ejercicio1.pas
@@ -1,3 +1,7 @@
+Implemente un programa que lea por teclado dos números enteros e imprima en pantalla los
+valores leídos en orden inverso. Por ejemplo, si se ingresan los números 4 y 8, debe mostrar el
+mensaje: Se ingresaron los valores 8 y 4
+
 program EJ1P0;
 var 
     num1,num2:integer;
