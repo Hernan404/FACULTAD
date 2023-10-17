eb565f1936b1cc0adb1ef1f191191e1da88756e2
Hernan <83614099+Hernan404@users.noreply.github.com>
2023-10-17 09:17:33 -0300

    Update Ejercicio6.pas

--- CADP/PRACTICA 7/Ejercicio6.pas
+++ CADP/PRACTICA 7/Ejercicio6.pas
@@ -1,185 +1,163 @@
 
-El Observatorio Astronómico de La Plata ha realizado un relevamiento sobre los distintos objetos 
-astronómicos observados durante el año 2015. Los objetos se clasifican en 7 categorías: 1: estrellas, 
-2: planetas, 3: satélites, 4: galaxias, 5: asteroides, 6: cometas y 7: nebulosas.
-Al observar un objeto, se registran los siguientes datos: código del objeto, categoría del objeto (1..7), 
-nombre del objeto, distancia a la Tierra (medida en años luz), nombre del descubridor y año de su 
-descubrimiento. 
-
-A. Desarrolle un programa que lea y almacene la información de los objetos que han sido 
-observados. Dicha información se lee hasta encontrar un objeto con código -1 (el cual no debe 
-procesarse). La estructura generada debe mantener el orden en que fueron leídos los datos.
-
-B. Una vez leídos y almacenados todos los datos, se pide realizar un reporte con la siguiente
-información:
-
-1. Los códigos de los dos objetos más lejanos de la tierra que se hayan observado.
-2. La cantidad de planetas descubiertos por "Galileo Galilei" antes del año 1600.
-3. La cantidad de objetos observados por cada categoría.
-4. Los nombres de las estrellas cuyos códigos de objeto poseen más dígitos pares que 
-impares
-
-
 program EJ6P7;
+type 
+    objetos = record 
+        codigo:integer;
+        categoria:1..7;
+        nombre_objeto:string;
+        distancia:integer;
+        nombre_descubridor:string;
+        anio_descubrimiento:integer;
+    end;
 
-type
-  rangoCategoria = 1..7;
+    lista = ^nodo;
+    nodo = record 
+        data:objetos;
+        sig:lista;
+    end;
 
-  objetos = record
-    codigo: integer;
-    categoria: rangoCategoria;
-    nombre_planeta: string;
-    distancia: integer;
-    nombre_descubridor: string;
-    aniodesc: integer;
-  end;
+    vector = array [1..7] of integer;
 
-  vector = array [rangoCategoria] of integer;
-
-  lista = ^nodo;
-  nodo = record
-    datos: objetos;
-    sig: lista;
-  end;
-
-procedure leer(var o: objetos);
-begin
-  with o do
-  begin
-    readln(codigo);
-    readln(categoria);
-    readln(nombre_planeta);
-    readln(distancia);
-    readln(nombre_descubridor);
-    readln(aniodesc);
-  end;
+procedure inivector (var v:vector);
+var 
+    i:integer;
+begin 
+    for i:=1 to 7 do begin 
+        v[i]:= 0;
+    end;
 end;
 
-procedure armarnodo(var pri: lista; o: objetos);
-var
-  act, aux: lista;
-begin
-  new(aux);
-  aux^.datos := o;
-  aux^.sig := nil;
-  if (pri <> nil) then
-  begin
-    act := pri;
-    while (act^.sig <> nil) do
-      act := act^.sig;
-    act^.sig := aux;
-  end
-  else
-    pri := aux;
+procedure leer(var o:objetos);
+begin 
+    with o do begin 
+        readln(codigo);
+        if (codigo <> -1) then begin 
+        readln(categoria);
+        readln(nombre_objeto);
+        readln(distancia);
+        readln(nombre_descubridor);
+        readln(anio_descubrimiento);
+        end;
+    end;
 end;
 
-procedure inivector(var v, vc: vector);
-var
-  i: rangoCategoria;
-begin
-  for i := 1 to 7 do
-  begin
-    v[i] := 0;
-    vc[i] := 0;
-  end;
+//crear lista ordenada por los datos leidos
+procedure armarlista (L:lista; o:objetos);
+var 
+    aux,pos:lista;
+begin 
+    new(aux);
+    aux^.data:= o;
+    aux^.sig:= L;
+    
+    if (L = nil) then 
+        L:= aux
+    else begin 
+        pos:= L;
+        while (pos^.sig <> nil) do
+            pos:= pos^.sig;
+
+        pos^.sig:= aux;
+    end;
 end;
 
-procedure cargarlista(var L: lista);
-var
-  o: objetos;
-begin
-  leer(o);
-  while (o.codigo <> -1) do
-  begin
-    armarnodo(L, o);
+procedure cargardatos (var L:lista);
+var 
+    o:objetos;
+begin 
     leer(o);
-  end;
+    while (o.codigo <> -1) do begin 
+        armarlista(L,o);
+        leer(o);
+    end;
+end;    
+
+procedure punto1 (var max1,max2,p1,p2:integer; var o:objetos);
+begin 
+    if (o.distancia > max1) then begin 
+        max2:=max1;
+        p2:= p1;
+        max1:= o.distancia;
+        p1:= o.codigo;
+    end
+    else 
+        if (max2 > max1) then begin 
+            max2:= o.distancia;
+            p2:= o.codigo;
+        end;
 end;
 
-procedure punto1(var o: objetos; var cumplemax2, cumplemax1: integer; var max1, max2: integer);
-begin
-  if (o.distancia > max1) then
-  begin
-    max2 := max1;
-    cumplemax2 := cumplemax1;
-    cumplemax1 := o.codigo;
-    max1 := o.distancia;
-  end
-  else if (o.distancia > max2) then
-  begin
-    max2 := o.distancia;
-    cumplemax2 := o.codigo;
-  end;
+function punto2 (o:objetos):boolean;
+begin 
+    punto2:= (o.nombre_descubridor = 'galileo galilei') and (o.anio_descubrimiento < 1600);
 end;
 
-function cumple2(o: objetos): boolean;
-begin
-  cumple2 := (o.nombre_descubridor = 'galileo galilei') and (o.aniodesc < 1600);
+function punto4 (codigo:integer):boolean;
+var 
+    par,impar:integer;
+begin 
+    par:= 0;
+    impar:= 0;
+
+    while (codigo > 0) do begin 
+        if (codigo mod 2 = 0) then 
+            par:= par +1
+        else 
+            impar:= impar +1;
+        codigo:= codigo div 10;
+    end;
+    punto4:= (par > impar);
 end;
 
-function punto4(o: objetos): boolean;
-var
-  digitos, par: integer;
-begin
-  par := 0;
+procedure procesardatos(L:lista);
+var 
+    i,cantpunto2,cantpunto4,codaux,max1,max2,p1,p2:integer;
+    o:objetos;
+    v:vector;
+begin 
+    max1:= -1;
+    max2:= -1;
+    p1:= 0;
+    p2:= 0;
+    codaux:= 0;
+    cantpunto2:= 0;
+    cantpunto4:= 0;
+    inivector(v);
 
-  while (o.codigo <> 0) do
-  begin
-    digitos := o.codigo mod 10;
-    if (digitos mod 2 = 0) then
-      par := par + 1;
-    o.codigo := o.codigo div 10;
-  end;
+    while (L <> nil) do begin 
+        codaux:= codaux + L^.data.codigo;
+        cantpunto2:= 0;
+        while (L <> nil) and (L^.data.codigo = codaux) do 
+            //punto1 
+            punto1(max1,max2,p1,p2,o);
 
-  punto4 := (par > (8 - par));
+            //punto2 
+            if punto2(L^.data) and (L^.data.categoria = 2) then 
+                cantpunto2:= cantpunto2 +1;
+            
+            //punto3
+            v[L^.data.categoria]:= v[L^.data.categoria] + 1;
+
+            //punto4 
+            if punto4(L^.data.codigo) and (L^.data.categoria = 1) then 
+                cantpunto4:= cantpunto4 +1;
+            
+            L:= L^.sig;
+        end;
+    
+    writeln('punto1: ', p1,p2);
+    writeln('punto2: ', cantpunto2);
+        for i:= 1 to 7 do 
+            writeln('punto3: ', v[i], i);
+    writeln('punto4: ', cantpunto4); 
 end;
 
-procedure procesarLista(L: lista);
-var
-  vc: vector;
-  v: vector;
-  {o: objetos;}
-  max1, max2: integer;
-  cantcumple2: integer;
-  cumple4: string;
-  cumplemax1: integer;
-  cumplemax2: integer;
-  i: integer;
-begin
-  cantcumple2 := 0;
-  max1 := -1;
-  max2 := -1;
-  cumplemax1 := 0;
-  cumplemax2 := 0;
-
-  inivector(v, vc);
-  while (L <> nil) do
-  begin
-    vc[L^.datos.categoria] := vc[L^.datos.categoria] + 1;
-
-    punto1(L^.datos, cumplemax1, cumplemax2, max1, max2);
-
-    if (cumple2(L^.datos)) then
-      cantcumple2 := cantcumple2 + 1;
-
-    if (punto4(L^.datos)) then
-      cumple4 := L^.datos.nombre_planeta;
-
-    L := L^.sig;
-  end;
-
-  writeln('Los códigos de los dos objetos más lejanos son: ', cumplemax1, ' ', cumplemax2);
-  writeln('La cantidad de planetas descubiertos por Galileo Galilei antes del año 1600 es: ', cantcumple2);
-  for i := 1 to 7 do
-    writeln('La cantidad de objetos en la categoría ', i, ': ', vc[i]);
-  writeln('El nombre de las estrellas con más dígitos pares que impares: ', cumple4);
-end;
-
-var
-  L: lista;
-begin
-  L := nil;
-  cargarlista(L);
-  procesarLista(L);
+var 
+    L:lista;
+begin 
+    L:= nil;
+    cargardatos(L);
+    procesardatos(L);
 end.
+
 
