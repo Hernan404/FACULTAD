e4c4b1a9125e9254f28ce83f9ebdd25c05475cb9
Hernan <83614099+Hernan404@users.noreply.github.com>
2023-10-18 18:03:56 -0300

    Update Ejercicio9.pas

--- CADP/PRACTICA 7/Ejercicio9.pas
+++ CADP/PRACTICA 7/Ejercicio9.pas
@@ -1,86 +1,110 @@
 program EJ9P7;
 type 
-    rango = 1..8
-    pelicula = record 
-        codigoPelicula:integer;
-        tituloPelicula:integer;
-        codigoGenero:rango;
-        puntajePromedio:real;
+    peliculas = record 
+        codigo:integer;
+        titulo:string;
+        codigo:1..8;
+        puntaje_promedio:integer;
     end;
 
-    vector array [rango] of integer;
+    criticas = record 
+        DNI:integer;
+        apellidoYnombre:string;
+        codigo:integer;
+        puntaje:integer;
+    end;
 
-procedure leer ();
-begin 
-    writeln('DNI del critico');
-    readln(DNI);
-    writeln('apellido y nombre');
-    readln(apellidoYnombre);
-    writeln('codigo de pelicula');
-    readln(codigo_pelicula);
-    writeln('puntaje otorgado');
-    readln(puntaje);
-end;
+    lista = ^nodo
+    nodo = record 
+        data:peliculas;
+        sig:lista;
+    end;
 
-procedure actualizar (var pri:lista; d:pelicula);
+    vector = array [1..8] of integer;
+
+procedure inivector (v:vector; p:peliculas);
 var 
-    ant,nue,act:lista;
+    i:integer;
 begin 
-    new(nue);
-    nue^.datos:= per;
-    act:= pri;
-    ant:= pri;
-
-    while (act <> nil) and (act^.datos.codigo < d.codigo) do begin 
-        ant:= act;
-        act:= act^.sig;
+    for i:=1 to 8 do begin 
+        v[i]:= 0;
     end;
-    if (ant = act) then 
-        pri:= nue;
-    else 
-        ant^.sig:= nue;
-    nue^.sig := act;
-end;
+end;    
 
-procedure maspuntaje ();
+procedure cargardatos (L:lista);
+var 
+    c:criticas;
+    p:peliculas;
 begin 
-    if (d.puntaje > max) then begin 
-        max:= d.puntaje;
-        puntoB:= d.codigoGenero;
-    end;
+    leer(c);
+    leer(p);
+    repeat 
+        armarlistapelis(L,p);
+        leer(c);
+        armarlista(L,c);
+        leer(p);
+    until c.codigo = -1;
 end; 
 
-procedure par (num:longint; var par,impar:integer);
-var 
-    digitos:integer;
+procedure puntoB(var p1,max:integer; var p:peliculas);
 begin 
-    digitos:= DNI mod 10;
-        if (digitos div 2 = 0) then 
-            par:= digitos;
+    if (c.puntaje > max) then begin 
+        max:= c.puntaje;
+        p1:= v[L^.data.codigo];
+    end;
+end;
+
+procedure puntoC(c:criticas):boolean
+var 
+    digito,par,impar:integer;
+begin 
+    par:= 0;
+    impar:= 0;
+
+    while (digito > 0) do begin 
+        digito:= c.DNI mod 10;
+
+        if (digito mod 2 = 0) then 
+            par:= par +1;
+        end 
         else 
             impar:= impar +1;
-    if (par > impar) then 
-        writeln('nombre con mas pares' apellidoYnombre);
-        
-    DNI:= DNI DIV 10;
+    end;
+
+    puntoC:= (par = impar);
 end;
 
-procedure procesarDatos ();
-begin  
-    inivector();
-    inilista();
-    leer();
-    actualizar();
+procedure procesarDatos(L1,L2:lista);
+var 
+    v:vector;
+    p:peliculas;
+    cantcriticas:integer;
+begin
+    cantcriticas:= 0;
+    while (L <> nil) do begin
+        //punto 1 
+        cantcriticas:= cantcriticas +1;
+        L1^.data.puntaje_promedio^:= L2^.data.puntaje + L1^.data.puntaje_promedio;
+
+        //punto 2 
+        puntoB(max,p1,p);
+
+        //punto 3
+        if puntoC(c:criticas) then
+            cumpleC:= L^.data.apellidoYnombre; 
+    end;
+    //punto 4
+    eliminar();
+end; 
+
+
 
 var 
-    d:pelicula;
-    v:vector;
-    max:integer;
-begin
-    max:= -1;
-    inicializarVector();
-    cargarDatos();
+    L:lista;
+    c:criticas;
+    p:peliculas;
+begin 
+    L:= nil;
+    cargardatos(L);
+    procesarDatos(L);
 end;
-
-
-
