91f94faa5afff2fd6c6dd74317a5b76721dc0ad8
Hernan <83614099+Hernan404@users.noreply.github.com>
2023-10-20 13:45:28 -0300

    Update Ejercicio1.pas

--- CADP/PRACTICA 7/Ejercicio1.pas
+++ CADP/PRACTICA 7/Ejercicio1.pas
@@ -1,3 +1,4 @@
+
 program EJ1P7;
 type 
     casting = record 
@@ -7,7 +8,7 @@ type
         codigo:1..5;
     end;
 
-    lista = ^nodo 
+    lista = ^nodo;
     nodo = record 
         data:casting;
         sig:lista;
@@ -36,7 +37,7 @@ end;
 
 procedure leer(var c:casting);
 begin 
-    whith c do begin
+    with c do begin
         readln(DNI);
         readln(apellidoYnombre);
         readln(edad);
@@ -44,37 +45,41 @@ begin
     end; 
 end;
 
-procedure cargardatos(L:lista; c:casting);
+procedure cargardatos(L:lista);
+var 
+	c:casting;
 begin 
     leer(c);
      repeat
         leer(c);
         armarlista(L,c);
-     until (c.DNI <> 3355); 
-    end;
+     until (c.DNI <> 33555444); 
 end;
 
-function puntoA (L:lista):boolean;
+function puntoA (num:integer):boolean;
 var 
     digito,par,impar:integer;
 begin
-    digito:= c.DNI mod 10;
-    while (c.DNI > 0) do begin 
-    if (digito mod 2 = 0) then 
-        par:= par +1;
-    else 
-        impar:= impar +1;
-    end;
+	par:= 0;
+	impar:= 0;
+	digito:= num mod 10;
+	while (digito > 0) do begin 
+			
+		if (digito mod 2 = 0) then begin  
+			par:= par +1;
+		end
+		else begin 
+			impar:= impar +1;
+		end;
 
-    c.DNI:= c.DNI div 10;
-    
-    if (par > impar) then 
-        puntoA:= true;
-    else 
-        puntoA:= false;
+		num:= digito div 10;
+		
+	end;
+	puntoA:= (par > impar);
+	
 end;
 
-procedure eliminar (var pri:lista; c:casting; var exito:boolean);
+procedure eliminar (var pri:lista; var c:casting; var exito:boolean);
 var 
     dni:integer;
     ant,act:lista;
@@ -94,49 +99,65 @@ begin
     if (act <> nil) then begin 
         exito:= true;
         if(act = pri) then 
-            pri:= act^.sig;
+            pri:= act^.sig
         else 
             ant^.sig:= act^.sig;
         dispose(act);
     end;
 end;
 
-procedure puntoB (v:vector ;var max1,max2:integer; var p1,p2:integer);
+procedure puntoB (v:vector ;var max1,max2,p1,p2:integer);
 var 
     i:integer;
 begin  
-    max1:= -1;
-    max2:= -1;
+    
 
     for i:= 1 to 5 do begin
         if (v[i] > max1) then begin
             max2:= max1;
-            p1:= i;
+            p2:= p1;
+            max1:= i;
+            p1:= v[i];
+
+        end 
+        else begin 
             if (v[i] > max2) then 
-                max2:= v[i];
+                max2:= i;
+                p2:= v[i];
+        end;
     end;
 end;
 
 procedure procesarDatos(L:lista);
 var 
-    cantA,cantB,codACT:integer;
+	v:vector;
+	c:casting;
+    max1,max2,p1,p2,cantA:integer;
+    exito:boolean;
 begin 
+	max1:= -1;
+    max2:= -1;
+    p1:= 0;
+    p2:= 0;
+    
     inivector(v);
     cantA:= 0;
     while (L <> nil) do begin 
-        if puntoA(L^.data) then 
+        if puntoA(L^.data.DNI) then 
             cantA:= cantA +1;
-        puntoB();
+        puntoB(v,max1,max2,p1,p2);
+
+	L:= L^.sig;
     end;
-    eliminar(L,c);
+    writeln('cantidad de personas con DNI pares: ', cantA);
+    writeln('los dos codigos de genero: ', p1,p2);
+    eliminar(L,c,exito);
 end;
 
 var 
-    v:vector;
-    c:casting;
     L:lista;
 begin 
     L:= NIL;
-    cargardatos(L,c);
+    cargardatos(L);
     procesarDatos(L);
 end.
