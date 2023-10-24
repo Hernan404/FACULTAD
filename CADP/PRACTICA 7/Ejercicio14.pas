354bd8f0e6d7879533f22534ec7e6886bb3406ab
Hernan <83614099+Hernan404@users.noreply.github.com>
2023-10-24 18:20:47 -0300

    Update Ejercicio14.pas

--- CADP/PRACTICA 7/Ejercicio14.pas
+++ CADP/PRACTICA 7/Ejercicio14.pas
@@ -1,111 +1,98 @@
 program EJ14P7;
+const 
+    dia = 31;
 type 
-    prestamos = record 
+    rango_dia:= 1..31;
+
+    prestamo = record 
         numero:integer;
         ISBN:integer;
-        numsocio:integer;
-        dia:1..31;
-    end;
+        numero_socio:integer;
+        diaprestamo:rango_dia;
+    end; 
 
-    vector = array [1..31] of integer;
-
-    lista = nodo 
+    lista =  ^nodo 
     nodo = record 
-        datos:prestamos;
+        data:prestamo;
         sig:lista;
     end;
 
-procedure iniciovector(v); //se dispone
-procedure cargarlista(L); // se dispone
-procedure leer(p); // se dispone
-procedure insertarOrdenado(var pri:lista; p:prestamos);
-var 
-    ant,nue,act:lista;
-begin 
-    new(nue);
-    nue^.datos:= p;
-    act := pri;
-    ant := pri;
+    vector = array [rango_dia] of integer;
 
-    while (act <> nil) and (act.datos.IBSN < p.IBSN) do begin 
-        ant:= act;
-        act:= act^.sig;
+procedure leer(r:prestamo);
+
+procedure armarlista(L:lista; r:prestamo);
+
+procedure inivector (var v:vector); 
+
+procedure cargardatos (L:lista);
+var 
+    r:prestamo;
+begin 
+    leer(r);
+    while (ISBN <> -1) do begin 
+        armarlista(L,r);
+        leer(r);
     end;
+end; 
 
-    if (ant = act) then 
-        pri:= nue;
-    else 
-        ant^.sig:= nue;
-    nue^.sig:= act;
-
-
-procedure puntoB (vcantprestamos:vector; min:integer; var cumpleB:integer);
-var 
-    i:1..31;
+procedure puntoB (var max,p1:integer; v:vector);
 begin 
-    min:= 9999;
-    for i:=1 to 31 do begin  
-        if (vcantprestamos[i] < min) then begin 
-            min:= vcantprestamos[i];
-            cumpleB:= i;
+    for i:=1 to dia do begin 
+        if (v[i].dia < min) then begin 
+            min:= v[i];
+            p1:= i;
         end;
-end;
-
-function puntoC (p:prestamos):boolean;
-var 
-    cumple1,cumple2:boolean; 
-    par1,impar1,impar2,par2:integer;
-begin 
-    cumple1:= false;
-    cumple2:= false;
-    par1:= 0;
-    impar1:= 0;
-    par2:= 0;
-    impar1:= 0;
-    if (numero mod 2 = 0) then 
-        par1:= +1;
-    else 
-        impar1:= impar1 +1;
-    
-    cumple1:= (par = 0) and (impar <> 0);
-
-    if (cumple1) then begin 
-        if (p.numsocio mod 2 = 0) then 
-            par2:= par2 +1;
-        else 
-            impar2:= impar2 +1;
-        
-        cumple2:= (impar = 0) and (par <> 0);
     end;
-    puntoC:= (cumple1) and (cumple2); 
 end;
 
-procedure procesardatos (L:lista);
+function puntoC(num:integer):boolean;
+var 
+    digito,par,impar:integer;
+begin 
+    par:= 0;
+    impar := 0;
+end;    
+
+procedure procesardatos(L:lista);
 var 
     v:vector;
-    p:prestamos;
-    cantcumpleC:integer;
+    r:prestamo;
+    min,p1:integer;
 begin 
-    cantcumpleC:= cantcumpleC +1;
-    while (L <> nil) do begin
+    min:= 9999;
+    p1:= 0;
+    suma:= suma +1;
+    while (L <> nil) do begin 
+    ISBNact:= L^.data.ISBN;
+        while (L <> nil) and (L^.data.ISBN = ISBNact) do begin 
+            //punto A
+            v[L^.data.dia]:= v[L^.data.dia] +1;
 
-        //inciso B
-        vcantprestamos[L.datos.dia]:= vcantprestamos[L.datos.dia] + 1
-        puntoB(vcantprestamos,min,cumpleB);
+            //punto B
+            puntoB(max1,p1,v);
 
-        //inciso C 
-        if (cumpleC(L.datos)) then begin 
-            cantcumpleC:= cantcumpleC +1;
+            //punto C 
+            suma:= suma +1;
+            if ((L^.data.numero mod 10) mod 2 = 1) and ((L^.data.numero_socio mod 10) mod 2 = 0) then 
+                cantC:= cantC +1;
+
+            L:= L^.sig;
         end;
+        for i:=1 to dia do begin 
+            writeln('punto A', v[i], i);
+        end; 
+        writeln('punto B', p1);
+        writeln('punto C', cantC*100/suma);
     end;
-    writeln('punto B', cumpleB);
-    writeln('punto C', cantcumpleC);
 
 end;
+
 
 var 
     L:lista;
 begin 
-    cargarlista(L);
+    L:= nil;
+    cargardatos(L);
     procesarDatos(L);
 end.
