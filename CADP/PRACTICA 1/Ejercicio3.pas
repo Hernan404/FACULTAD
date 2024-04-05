9803f2693cb9cc51afb075ae4ab85394719846f3
Hernan <83614099+Hernan404@users.noreply.github.com>
2024-04-05 14:20:04 -0300

    Update Ejercicio3.pas

--- CADP/PRACTICA 1/Ejercicio3.pas
+++ CADP/PRACTICA 1/Ejercicio3.pas
@@ -1,19 +1,24 @@
-{Realizar un programa que lea 3 números enteros y los imprima en orden descendente.
-}
+Realizar un programa que lea 3 números enteros y los imprima en orden descendente.
 program ej3p1;
+var 
+    num1,num2,num3:integer;
+begin 
+    read(num1) 
+    read(num2)
+    read(num3) 
 
-var
-	NumeroReal: real;
-	DobleReal: real;
-begin
-	NumeroReal:=0;
-	DobleReal:=0;
-	write('Ingrese un numero real: ');
-	read(NumeroReal);
-	while ((DobleReal/2)<>NumeroReal) do
-		begin
-			read(DobleReal);
-			writeln('Su numero es ',DobleReal:2:1);
-		end;
-	write('El numero que es el doble del real es: ', DobleReal:2:1);
+    if (num1 > num2) and (num1 > num3) and (num2 > num3) then
+        write(num1,num2,num3)
+    else if (num3 > num3) then 
+        write(num1,num2,num3)
+    
+    if (num2 > num1) and (num2 > num3) and (num1 > num3) then 
+        write(num2,num1,num3)
+    else if (num3 > num1) then 
+        write(num2,num3,num1)
+    
+    if (num3 > num1) and (num3 > num2) and (num2 > num1) then 
+        write(num3, num2, num1)
+    else if (num1 > num2) then 
+        write(num3,num1,num2)
 end.
