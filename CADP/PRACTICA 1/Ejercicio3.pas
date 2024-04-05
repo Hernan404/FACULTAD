70f645b3f83df48e18756a3c2db1ffe33ee04ee7
Hernan <83614099+Hernan404@users.noreply.github.com>
2024-04-05 14:33:16 -0300

    Update Ejercicio3.pas

--- CADP/PRACTICA 1/Ejercicio3.pas
+++ CADP/PRACTICA 1/Ejercicio3.pas
@@ -7,18 +7,27 @@ begin
     read(num2)
     read(num3) 
 
-    if (num1 > num2) and (num1 > num3) and (num2 > num3) then
-        write(num1,num2,num3)
-    else if (num3 > num3) then 
-        write(num1,num2,num3)
-    
-    if (num2 > num1) and (num2 > num3) and (num1 > num3) then 
-        write(num2,num1,num3)
-    else if (num3 > num1) then 
-        write(num2,num3,num1)
-    
-    if (num3 > num1) and (num3 > num2) and (num2 > num1) then 
-        write(num3, num2, num1)
-    else if (num1 > num2) then 
-        write(num3,num1,num2)
+    if (num1 > num2) and (num1 > num3) then 
+    begin 
+        if (num2 > num3) then 
+            writeln(num1, num2, num3) 
+        else 
+            write(num1, num3, num2)
+    end;
+
+    else if (num2 > num1) and (num2 > num3) 
+    begin 
+        if (num1 > num3) then 
+            writeln(num2, num1, num3) 
+        else 
+            writeln(num2, num3, num1)
+    end
+
+    else
+    begin 
+        if (num1 > num2) then 
+            writeln(num3, num1, num2)
+        else 
+            writeln(num3, num2, num1)
+    end;
 end.
