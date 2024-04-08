e67ae245f829ff0bb5001bfcbb52eff204309acf
Hernan <83614099+Hernan404@users.noreply.github.com>
2024-04-08 11:28:28 -0300

    Create Ejercicio7.pas

---
+++ CADP/PRACTICA 1/PARTE 2/Ejercicio7.pas
@@ -0,0 +1,46 @@
+program EJ87P1P2;
+var 
+    nombre:string;
+    tiempo:real;
+    max1,max2,min1,min2:real;
+    p1,p2,u1,u2:string;
+    i:integer;
+begin 
+   
+    max1:= -1;
+    max2:= -1;
+    min1:= 9999;
+    min2:= 9999;
+    
+    
+
+    for i:=1 to 4 do begin 
+    
+		readln(nombre);
+		readln(tiempo);
+		
+        if (tiempo > max1) then begin 
+            max2:= max1;
+            p2:= p1;
+            max1:= tiempo;
+            p1:= nombre;
+        end 
+        else if (max2 < max1) then begin 
+                max2:= tiempo;
+                p2:= nombre;
+        end;
+
+        if (tiempo < min1) then begin 
+            min2:= min1;
+            u2:= u1;
+            min1:= tiempo;
+            u1:= nombre;
+        end 
+        else  if (tim1 < min2) then begin  
+                min2:= tiempo;
+                u2:= nombre;
+        end;
+    end;
+    writeln('los primeros puestos son ', p1 ,'y', p2);
+    writeln('los ultimos puestos son ', u1 ,'y', u2); 
+end.
