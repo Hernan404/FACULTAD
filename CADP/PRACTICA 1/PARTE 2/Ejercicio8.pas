60653d8805b601bf4045df69d16e60dd0547cb3f
Hernan <83614099+Hernan404@users.noreply.github.com>
2024-04-08 11:54:59 -0300

    Update Ejercicio8.pas

--- CADP/PRACTICA 1/PARTE 2/Ejercicio8.pas
+++ CADP/PRACTICA 1/PARTE 2/Ejercicio8.pas
@@ -38,3 +38,39 @@ begin
 	Writeln('El dia con mayor cantidad de ventas ', mayor_ventas);
 	Writeln('La cantidad del mes es: ', cant_mes);
 end.
+
+Un local de ropa desea analizar las ventas realizadas en el último mes. Para ello se lee por cada día del mes,
+los montos de las ventas realizadas. La lectura de montos para cada día finaliza cuando se lee el monto 0. Se
+asume un mes de 31 días. Informar la cantidad de ventas por cada día, y el monto total acumulado en ventas
+de todo el mes.
+a. Modifique el ejercicio anterior para que además informe el día en el que se realizó la mayor cantidad
+de ventas     program EJ9P1P2;
+var 
+	monto:real;
+	dia:integer;
+	max:integer;
+	total:real;
+	ventas:integer;
+	p1:integer;
+begin 
+	max:= -1;
+	total:= 0;
+	ventas:= 0;
+
+	for dia:= 1 to 31 do begin 
+		readln(monto);
+		while (monto <> 0) do begin 
+			readln(monto);
+			ventas:= ventas +1;
+			total:= total + monto;
+			
+			if (ventas > max) then begin 
+				max:= ventas;
+				p1:= dia;
+			end;
+		end;
+		writeln('ventas del dia', ventas);
+	end;
+	writeln('monto total', total);
+	writeln('dia con mas ventas', p1);
+end.
