6e5d97c72620cf535cd9762b955e24b0bf0e6126
Hernan <83614099+Hernan404@users.noreply.github.com>
2024-04-08 10:33:00 -0300

    Update Ejercicio4.pas

--- CADP/PRACTICA 1/PARTE 2/Ejercicio4.pas
+++ CADP/PRACTICA 1/PARTE 2/Ejercicio4.pas
@@ -1,70 +1,41 @@
-program EJ4P1P2;
+Realizar un programa que lea 1000 números enteros desde teclado. Informar en pantalla cuáles son los dos
+números mínimos leídos.
 
-type
-	bucle=0..10;
-
-var
-	i:bucle;
-	numero_uno,numero_dos,chico,chico2:integer;
-begin
-	numero_uno:=0;
-	numero_dos:=0;
-	chico:=0;
-	chico2:=0;
-	read(chico);
-	for i:=0 to 10 do 
-		begin
-			write('Ingrese un numero: ');
-			read(chico);
-			if(chico>chico2)then
-				chico:=chico2;
-			if(chico<chico2)then
-				chico2:=chico;
-		end;
-	
-	if(chico<chico2) then
-		begin
-			writeln('el numero mas chico es',chico);
-			writeln('el segundo numero mas chico es',chico2);
-		end
-	else
-		begin
-			writeln('el numero mas chico es',chico2);
-			writeln('el segundo numero mas chico es',chico);
-		end;
-	
+program EJ9P1P1;
+var 
+    car:char;
+    num:integer;
+    operacion:integer;
+begin 
+    readln(char);
+    if (char = '+') or (char = '-') then begin 
+        while (num <> 0) do begin 
+            readln(num);
+            if (char = '+') then 
+                operacion:= operacion + num
+            else 
+                operacion:= operacion - num;
+        end;
+    end
+    else 
+        write('error');
 end.
 
-
-
-
-Program EJ5P1P2;
-
-var
-
-	num,min,max,suma:integer;
-
-begin
-	max:= -9999;
-	min:= 9999;
-	suma:= 0;
-
-	repeat
-		readln(num);
-		
-		suma:= suma+num;
-		
-		if (num > max) then
-			max:= max+num;
-		if (num < min) then
-			min:= min+num;
-		
-	
-	until num = 100;
-
-	writeln('el maximo es ' max);
-	writeln('el minimo es ' min);
-	writeln('la suma total es ' suma);
-	
-
-end
+program EJ4P1P2;
+var 
+    num,i,min1,min2:integer;
+begin 
+    min1:= 9999;
+    min2:= 9999;
+    for i:=1 to 1000 do begin 
+        read(num);
+        
+        if (num < min1) then  
+            min1:= num;
+            min2:= min1;
+        else 
+            if(min1 < min2) then 
+                min2:= num;
+    end;
+    write('numeros mas chicos ', min1 , min2);
+end.
