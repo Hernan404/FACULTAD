17d35ae3d66159ea70850aca3992374ceef05a72
Hernan <83614099+Hernan404@users.noreply.github.com>
2024-04-11 15:50:36 -0300

    Update Ejercicio5.pas

--- CADP/PRACTICA 1/PARTE 2/Ejercicio5.pas
+++ CADP/PRACTICA 1/PARTE 2/Ejercicio5.pas
@@ -1,27 +1,29 @@
+ Realizar un programa que lea números enteros desde teclado. La lectura debe finalizar cuando se ingrese el
+número 100, el cual debe procesarse. Informar en pantalla:
+● El número máximo leído.
+● El número mínimo leído.
+● La suma total de los números leídos.
+
 Program EJ5P1P2;
 
 var
-
-	num,min,max,suma:integer;
+   num,min,max,suma:integer;
 
 begin
-	max:= -1;
-	min:= 9999;
-	suma:= 0;
+   max:= -1;
+   min:= 9999;
+   suma:= 0;
 
-	repeat
-		readln(num);
+   repeat
+	readln(num);	
+	suma:= suma+num;
 		
-		suma:= suma+num;
-		
-		if (num > max) then
-			max:= num;
-		if (num < min) then
-			min:= num;
-		
-	
-	until num = 100;
-
+	if (num > max) then
+	    max:= num;
+	if (num < min) then
+	    min:= num;
+     
+until num = 100;
 	writeln('el maximo es ', max);
 	writeln('el minimo es ', min);
 	writeln('la suma total es ', suma);
