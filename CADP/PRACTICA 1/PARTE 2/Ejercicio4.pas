b63ab89ccd9d44552e358e8a04bb90f67e7d9fe3
Hernan <83614099+Hernan404@users.noreply.github.com>
2024-04-11 14:52:35 -0300

    Update Ejercicio4.pas

--- CADP/PRACTICA 1/PARTE 2/Ejercicio4.pas
+++ CADP/PRACTICA 1/PARTE 2/Ejercicio4.pas
@@ -1,41 +1,75 @@
 Realizar un programa que lea 1000 números enteros desde teclado. Informar en pantalla cuáles son los dos
 números mínimos leídos.
+a. Modifique el ejercicio anterior para que, en vez de leer 1000 números, la lectura finalice al leer el
+número 0, el cual debe procesarse.
+b. Modifique el ejercicio anterior para que, en vez de leer 1000 números, la lectura finalice al leer el
+número 0, el cual no debe procesarse.
 
-program EJ9P1P1;
+
+program EJ4P1P2; 
 var 
-    car:char;
     num:integer;
-    operacion:integer;
-begin 
-    readln(char);
-    if (char = '+') or (char = '-') then begin 
-        while (num <> 0) do begin 
-            readln(num);
-            if (char = '+') then 
-                operacion:= operacion + num
-            else 
-                operacion:= operacion - num;
-        end;
-    end
-    else 
-        write('error');
-end.
-
-program EJ4P1P2;
-var 
-    num,i,min1,min2:integer;
+    i:integer;
+    min1,min2:integer;
 begin 
     min1:= 9999;
     min2:= 9999;
+
     for i:=1 to 1000 do begin 
-        read(num);
-        
-        if (num < min1) then  
+        readln(num);
+        if (num < min1) then begin
             min1:= num;
             min2:= min1;
-        else 
-            if(min1 < min2) then 
+        end 
+        else if (min2 < min1) then begin  
                 min2:= num;
+        end;
     end;
-    write('numeros mas chicos ', min1 , min2);
+    writeln('los dos dos numeros minimos ', min1 , min2);
+end.
+
+// PUNTO A
+program EJ4P1P2; 
+var 
+    num:integer;
+    i:integer;
+    min1,min2:integer;
+begin 
+    min1:= 9999;
+    min2:= 9999;
+    repeat 
+        readln(num);
+        if (num < min1) then begin 
+            min1:= num;
+            min2:= min1;
+        end
+        else if (min2 < min1) then begin  
+                min2:= num;
+        end;
+    until num = 0;
+
+    writeln('los dos dos numeros minimos ', min1 , min2);
+end.
+
+// PUNTO B
+program EJ4P1P2; 
+var 
+    num:integer;
+    i:integer;
+    min1,min2:integer;
+begin 
+    min1:= 9999;
+    min2:= 9999;
+
+    while (num <> 0) do begin 
+        readln(num);
+        if (num < min1) then begin 
+            min1:= num;
+            min2:= min1;
+        end
+        else if (min1 < min2) then begin  
+                min2:= num;
+        end;
+    end;
+    writeln('los dos dos numeros minimos ', min1 , min2);
 end.
