cb6e0a3681a97e25c97ad903d3ce86c291ab5fd1
Hernan <83614099+Hernan404@users.noreply.github.com>
2024-04-12 17:40:27 -0300

    Update Ejercicio8.pas

--- CADP/PRACTICA 2/PARTE 2/Ejercicio8.pas
+++ CADP/PRACTICA 2/PARTE 2/Ejercicio8.pas
@@ -1,24 +1,17 @@
 program EJ8P2P2;
 
-procedure proceso (var num,sumapares,cantimpares:integer);
-var 
-	par,impar:integer;
+procedure proceso (num:integer; var sumapares,cantimpares:integer);
 begin 
-	impar:= 0;
+
 	while (num > 0) do begin 
-		par:= 0;
 		num:= num mod 10;
-		if (num mod 2 = 0) then begin 
-			par:= par + 1;
-			sumapares:= sumapares + num;
-		end
-		else begin 
-			impar:= impar +1;
-			cantimpares:= cantimpares + impar;
-		end;
+		if (num mod 2 = 0) then
+			sumapares:= sumapares + 1
+		else 
+			cantimpares:= cantimpares + 1;
+
 		num:= num div 10;
 	end;
-	writeln('SUMA DE LOS PARES DEL NUMERO: ', sumapares); 
 end;
 
 var 
@@ -29,10 +22,50 @@ begin
 	writeln('INGRESE NUMEROS');
 	readln(num);
 	while (num <> 123) do begin 
+		sumapares:=0;
+		cantimpares:= 0;
+		
 		proceso(num,sumapares,cantimpares);
+		writeln('CANTIDAD DE IMPARES TOTALES: ',cantimpares);
+		writeln('SUMA DE LOS PARES DEL NUMERO: ', sumapares); 
+		writeln();
 		writeln('INGRESE NUMEROS');
 		readln(num);
 	end;
-	writeln('CANTIDAD DE IMPARES TOTALES: ',cantimpares);
-end.
+	
+end.program EJ8P2P2;
 
+procedure proceso (num:integer; var sumapares,cantimpares:integer);
+begin 
+
+	while (num > 0) do begin 
+		num:= num mod 10;
+		if (num mod 2 = 0) then
+			sumapares:= sumapares + 1
+		else 
+			cantimpares:= cantimpares + 1;
+
+		num:= num div 10;
+	end;
+end;
+
+var 
+	num:integer;
+	sumapares:integer;
+	cantimpares:integer;
+begin 
+	writeln('INGRESE NUMEROS');
+	readln(num);
+	while (num <> 123) do begin 
+		sumapares:=0;
+		cantimpares:= 0;
+		
+		proceso(num,sumapares,cantimpares);
+		writeln('CANTIDAD DE IMPARES TOTALES: ',cantimpares);
+		writeln('SUMA DE LOS PARES DEL NUMERO: ', sumapares); 
+		writeln();
+		writeln('INGRESE NUMEROS');
+		readln(num);
+	end;
+	
+end.
