fd94cfea806ef50aa5a00aad0764a6a596ec1cb6
Hernan <83614099+Hernan404@users.noreply.github.com>
2024-04-12 17:26:21 -0300

    Update Ejercicio7.pas

--- CADP/PRACTICA 2/PARTE 2/Ejercicio7.pas
+++ CADP/PRACTICA 2/PARTE 2/Ejercicio7.pas
@@ -1,3 +1,9 @@
+ a. Realizar un módulo que reciba como parámetro un número entero y retorne la cantidad de dígitos que
+posee y la suma de los mismos.
+b. Utilizando el módulo anterior, realizar un programa que lea una secuencia de números e imprima la
+cantidad total de dígitos leídos. La lectura finaliza al leer un número cuyos dígitos suman exactamente 10,
+el cual debe procesarse.
+
 program EJ7P2P1;
 
 procedure proceso (var num,suma,total,cant:integer);
