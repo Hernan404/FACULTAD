30f94cb35ad4e3fe8125cdfc55304c3ff08f540d
Hernan <83614099+Hernan404@users.noreply.github.com>
2024-04-12 17:25:29 -0300

    Update Ejercicio8.pas

--- CADP/PRACTICA 2/PARTE 2/Ejercicio8.pas
+++ CADP/PRACTICA 2/PARTE 2/Ejercicio8.pas
@@ -1,73 +1,38 @@
 program EJ8P2P2;
-procedure leer (var precio:real; var codigo:integer; var tipo:string);
-begin
 
-		writeln('ingrese precio');
-		readln(precio);
-		writeln('ingrese codigo');
-		readln(codigo);
-		writeln('ingrese tipo de producto');
-		readln(tipo);	
-end
-
-
-procedure codigoBarato (precio:real; var cod_masbarato:real; codigo: integer; var min: real; tipo);
-begin
-if ( precio < min) then begin
-				min:= codigo;
-				cod_masbarato:= min;
-			
-			end;
-
-
-
+procedure proceso (var num,sumapares,cantimpares:integer);
+var 
+	par,impar:integer;
+begin 
+	impar:= 0;
+	while (num > 0) do begin 
+		par:= 0;
+		num:= num mod 10;
+		if (num mod 2 = 0) then begin 
+			par:= par + 1;
+			sumapares:= sumapares + num;
+		end
+		else begin 
+			impar:= impar +1;
+			cantimpares:= cantimpares + impar;
+		end;
+		num:= num div 10;
+	end;
+	writeln('SUMA DE LOS PARES DEL NUMERO: ', sumapares); 
 end;
 
-procedure pantaloncaro (precio:real; max:real; codigo:integer; var cod_pant_barato; tipo:string);
-begin
-if (tipo = 'pantalon') then begin
-				if (codigo > max) then
-					max:= codigo;
-					cod_pant_mascaro:= max;
-			end;
-end;
-var
-	precio:real;
-	codigo:integer;
-	tipo:string;
-	
-	producto:real;
-
-	
-	
-	cod_masbarato: real;
-	cod_pant_mascaro: real;
-	promedio: real;
-	
-	min,max: real;
-begin
-	producto:=0;
-	min:= 999;
-	max:= -1;
-	
-	repeat 
-		producto:= producto +1;
-		
-		
-			
-			
-			
-	until producto = 3;
-	
-		
-
-	writeln('producto mas barato', cod_masbarato);
-	writeln('pantalon mas caro', cod_pant_mascaro);
-	
-	promedio:= precio*3 / producto;
-	writeln('precio promedio', promedio);
-	 
-
-
-
+var 
+	num:integer;
+	sumapares:integer;
+	cantimpares:integer;
+begin 
+	writeln('INGRESE NUMEROS');
+	readln(num);
+	while (num <> 123) do begin 
+		proceso(num,sumapares,cantimpares);
+		writeln('INGRESE NUMEROS');
+		readln(num);
+	end;
+	writeln('CANTIDAD DE IMPARES TOTALES: ',cantimpares);
 end.
+
