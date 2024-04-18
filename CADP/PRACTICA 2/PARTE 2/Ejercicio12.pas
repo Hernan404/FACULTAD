f0ef0d3ab48b5c3d4e4d35bddacb7b4523903e88
Hernan <83614099+Hernan404@users.noreply.github.com>
2024-04-18 11:16:07 -0300

    Update Ejercicio12.pas

--- CADP/PRACTICA 2/PARTE 2/Ejercicio12.pas
+++ CADP/PRACTICA 2/PARTE 2/Ejercicio12.pas
@@ -1,52 +1,48 @@
-program EjerciciosAdd;
-procedure A(var vocalesf:char);
-var
-  signo_dinero:Boolean;
-begin
-  signo_dinero:=true;
-  
-  while (vocalesf = 'a') or (vocalesf = 'e') or (vocalesf = 'i') or (vocalesf = 'o') or (vocalesf = 'u') and (signo_dinero <> false) do
-      begin
-        readln(vocalesf);
-        if (vocalesf = '$') then
-          begin
-            signo_dinero:=false;
-            writeln('cumple hasta A');
-          end;
-      end;
-    if(signo_dinero=true) then
-      writeln('No se cumple en A');
-end;
-//___________________________________________________________________________________________
-procedure B(vocalesf:char);
-var
-  signo_numeral:Boolean;
-begin
-  signo_numeral:=true;
-  while (vocalesf <> 'a') and (vocalesf <> 'e') and (vocalesf <> 'i') and (vocalesf <> 'o') and (vocalesf <> 'u') and (signo_numeral<>false) do
-      begin
-        readln(vocalesf);
-        if(vocalesf = '#') then
-          begin
-            signo_numeral:=false;
-            write('Se cumple En B tambien');
-          end;
-       
-      end;
-  if(signo_numeral=true) then
-    begin
-      write('No se cumple en B');
+program EJ12P2P2;
+
+
+procedure proceso (var tipo_zona:integer; var precio,canthec:real);
+var 
+    t1,t2,t3,total:real;
+begin 
+	total:= 0;
+	t1:=0;
+	t2:=0;
+	t3:=0;
+    if (tipo_zona = 1) then begin 
+        t1:= canthec * 6;
+        t1:= t1 * precio;
     end;
+   
+    if (tipo_zona = 2) then begin 
+        t2:= canthec * 2.6;
+        t2:= t2 * precio;
+    end;
+    
+    if (tipo_zona = 3) then begin 
+        t3:= canthec * 1.4;
+        t3:= t3 * precio;
+	end;
+
+	total:= t1 + t2 + t3;
+    
+    writeln('RENDIMIENTO DE ZONA 1: ', t1:2:0);
+    writeln('RENDIMIENTO DE ZONA 2: ', t2:2:0);
+    writeln('RENDIMIENTO DE ZONA 3: ', t3:2:0);
+    writeln('RENDIMIENTO TOTAL: ', total:2:0);
 end;
-//___________________________________________________________________________________________
-var
-  vocales:char;
-  
-begin
-  vocales:='a';
-  writeln('Secuencia de solo letras vocales');
-  A(vocales);
-  writeln('secuencia de solo caracteres alfabeticos sin vocales: ');
-  B(vocales);
-     
+
+var 
+    canthec:real;
+    tipo_zona:integer;
+    precio:real;
+begin 
+    writeln('INGRESE CANTIDAD DE HECTAREAS');
+    readln(canthec);
+    writeln('INGRESE TIPO DE ZONA');
+    readln(tipo_zona);
+    writeln('INGRESE PRECIO');
+    readln(precio);
+
+    proceso(tipo_zona,precio,canthec);
 end.
