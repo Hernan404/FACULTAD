d0b00c49b51f3dffe5987a05a6cbe6bf8945c73e
Hernan <83614099+Hernan404@users.noreply.github.com>
2024-04-23 15:51:53 -0300

    Update Ejercicio2.pas

--- CADP/PRACTICA 3/Ejercicio2.pas
+++ CADP/PRACTICA 3/Ejercicio2.pas
@@ -1,51 +1,48 @@
+Implementar un programa que lea la fecha de todos los casamientos realizados en 2019. La lectura
+finaliza al ingresar el año 2020, que no debe procesarse, e informe la cantidad de casamientos
+realizados durante los meses de verano (enero, febrero y marzo) y la cantidad de casamientos
+realizados en los primeros 10 días de cada mes. Nota: utilizar el módulo realizado en b) para la lectura
+de fecha.
+
 program EJ2P3;
-
 type 
-	casamiento = record
-		dia: integer;
-		mes: string;
-		anio: integer;
-		cantCasamientosVerano: integer;
-		cantCasamientosPdias: integer;
+	fecha = record
+		dia:integer;
+		mes:1..12;
+		anio:integer;
 	end;
 
-procedure leer (var c:casamiento);
+procedure leer(var r:fecha);
 begin
-	writeln('dia');
-	readln(c.dia);
-	writeln('mes');
-	readln(c.mes);
-	writeln('anio');
-	readln(c.anio);
-end;
-
-procedure verano (var c:casamiento);
-begin
-	if (c.mes = 'enero') or (c.mes = 'marzo') or (c.mes = 'febrero') then begin
-		c.cantCasamientosVerano:= c.cantCasamientosVerano +1
+	with r do begin 
+		writeln('DIA');
+		readln(dia);
+		writeln('MES');
+		readln(mes);
+		writeln('ANIO');
+		readln(anio);
 	end;
 end;
 
-procedure primerosD (var c:casamiento);
-begin
-	if (c.dia <= 10) then begin
-		c.cantCasamientosPdias:= c.cantCasamientosPdias + 1;
-	end;
+procedure proceso (r:fecha; var cantv,cantdiez:integer);
+begin 
+	if (r.mes = 1) or (r.mes = 2) or (r.mes = 3) then 
+		cantv:=cantv +1;
+	if (r.dia < 10) then 
+		cantdiez:= cantdiez +1;
 end;
-
-var
-	c:casamiento;
-
-begin
-c.cantCasamientosVerano:= 0;
-c.cantCasamientosPdias:= 0;
-	while (c.anio <> 2020) do begin
-		leer(c);	
-		verano(c);
-		primerosD(c);
+var 
+	r:fecha;
+	cantv,cantdiez:integer;
+begin 
+	cantv:=0;
+	cantdiez:=0;
+	
+	leer(r);
+	while(r.anio <> 2020) do begin 
+		proceso(r,cantv,cantdiez);
+		leer(r);
 	end;
-
-	writeln('cantidad de casamientos de verano: ', c.cantCasamientosVerano);
-	writeln('cantidad de casamientos en los primeros 10 dias: ', c.cantCasamientosPdias);
+	writeln('cantidad de casamientos en verano: ',cantv);
+	writeln('cantidad de casamientos en los primeros 10 dias: ',cantdiez);
 end.
-
