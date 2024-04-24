90c3608164106cf319f8ac53af4d943e649326b8
Hernan <83614099+Hernan404@users.noreply.github.com>
2024-04-24 13:31:40 -0300

    Update and rename Ejercicio5.pas to Ejercicio5_FALTA_CORREJIR.pas

---
+++ CADP/PRACTICA 3/Ejercicio5_FALTA_CORREJIR.pas
@@ -0,0 +1,60 @@
+program EJ3P3;
+type 
+	datos = record 
+		marca:string;
+		modelo:string;
+		precio:real;
+end;
+
+
+procedure leer(var r:datos);
+begin 
+	with r do begin 
+		writeln('MARCA');
+		readln(marca);
+		writeln('MODELO');
+		readln(modelo);
+		writeln('PRECIO');
+		readln(precio);
+	end;
+end;
+
+procedure proceso (r:datos; m1,o1:string; var max:real);
+begin 	
+	if (r.precio > max) then begin 
+		max:= r.precio;
+		m1:= r.marca;
+		o1:= r.modelo;
+	end;
+end;
+
+var 
+	r:datos; 
+	marcaACT:string;
+	promedio,max:real;
+	m1,o1:string;
+	cant:integer;
+begin 
+	m1:= '';
+	o1:= '';
+	
+	max:= -1;
+	marcaACT:= '';
+	leer(r);
+	
+	while (r.marca <> 'ZZZ') do begin 
+		promedio:= 0;
+		cant:= 0;
+		marcaACT:= r.marca;
+		while (r.marca <> marcaACT) do begin 
+			promedio:= promedio + r.precio;
+			proceso(r,m1,o1,max); 
+			cant:= cant +1;
+			leer(r);
+		
+		end;
+	end;
+	
+	writeln('Marca y modelo del auto más caro. ', m1,o1);
+	writeln('El precio promedio por marca: ', promedio:2:0);
+end.
