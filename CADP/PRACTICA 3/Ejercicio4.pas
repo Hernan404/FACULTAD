e4a445df65be539f2b09cfbde909f89b1caf4e89
Hernan <83614099+Hernan404@users.noreply.github.com>
2024-04-24 12:40:13 -0300

    Update Ejercicio4.pas

--- CADP/PRACTICA 3/Ejercicio4.pas
+++ CADP/PRACTICA 3/Ejercicio4.pas
@@ -1,49 +1,56 @@
+
+Una compañía de telefonía celular debe realizar la facturación mensual de sus 9300 clientes con planes de
+consumo ilimitados (clientes que pagan por lo que consumen). Para cada cliente se conoce su código de
+cliente y cantidad de líneas a su nombre. De cada línea se tiene el número de teléfono, la cantidad de minutos
+consumidos y la cantidad de MB consumidos en el mes. Se pide implementar un programa que lea los datos
+de los clientes de la compañía e informe el monto total a facturar para cada uno. Para ello, se requiere:
+a. Realizar un módulo que lea la información de una línea de teléfono.
+b. Realizar un módulo que reciba los datos de un cliente, lea la información de todas sus líneas (utilizando
+el módulo desarrollado en el inciso a y retorne la cantidad total de minutos y la cantidad total de MB a
+facturar del cliente.
+Nota: para realizar los cálculos tener en cuenta que cada minuto cuesta $3,40 y cada MB consumido cuesta
+$1,35.
+
+
 program EJ4P3;
-
-type
-	telefono = record
-		numero:integer;
-		cantMin:real;
-		cantMB:real;
-		
-		
-		minutos:real;
-		MBs:real;
-		
-		Total_MB: real;
-		Total_minutos: real;
-	end;
-
-procedure leer (var t:telefono);
-begin
-	writeln('numero');
-	readln(t.numero);
-	writeln('cantidad de minutos');
-	readln(t.cantMin);
-	writeln('cantidad de MBs');
-	readln(t.cantMB);
+type 
+	datos = record 
+		codigo:integer;
+		cant_lineas:integer;
+		cant_min:real;
+		cant_mb:real;
 end;
 
-procedure consumido (var t:telefono);
-begin
-	t.Total_minutos:= 3.40 * t.cantMin;
-	t.Total_MB:= 1.35 * t.cantMB;
-end;
-
-var
-	t:telefono;
+procedure leer(var r:datos; var total_mb,total_min:real);
+var 
 	i:integer;
-begin
-	{t.minutos:= 3.40;
-	t.MBs:= 1.35;}
-	for i:=1 to 2 do begin
-		
-		leer(t);
-		consumido(t);
-		
-	writeln('cantida de minutos consumidos: ', t.Total_minutos);
-	writeln('cantidad de MBs consumidso: ', t.Total_MB);	
-		
+begin 
+	writeln('CODIGO');
+	readln(r.codigo);
+	writeln('CANTIDAD DE LINEAS DE TELEFONO');
+	readln(r.cant_lineas);
+	
+	for i:=1 to r.cant_lineas do begin 
+		writeln('CANTIDAD DE MINUTOS');
+		readln(r.cant_min);
+		writeln('CANTIDAD DE MBs');
+		readln(r.cant_mb);
+  
+		total_mb:= r.cant_mb + total_mb;
+		total_min:= r.cant_min + total_min;
+	end;
+end;
+
+var 
+	r:datos;
+	i:integer;
+	total_mb,total_min:real;
+begin 
+	
+	for i:=1 to 9300 do begin 
+		leer(r,total_mb,total_min);
 	end;
 	
+	writeln('CANTIDAD DE MBs TOTAL A FACTURAR: ', total_mb * 1.35:2:0,'$');
+	writeln('CANTIDAD DE MINUTOS TOTAL A FACTURAR: ', total_min * 3.40:2:0,'$');
 end.
