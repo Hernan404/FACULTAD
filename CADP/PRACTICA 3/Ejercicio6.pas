f31f91f1ded9f6dc73d3bda710ba542b803724ff
Hernan <83614099+Hernan404@users.noreply.github.com>
2024-04-25 11:41:55 -0300

    Update Ejercicio6.pas

--- CADP/PRACTICA 3/Ejercicio6.pas
+++ CADP/PRACTICA 3/Ejercicio6.pas
@@ -1,16 +1,15 @@
 Una empresa importadora de microprocesadores desea implementar un sistema de software para analizar la
-información de los productos que mantiene actualmente en stock. Para ello, se conoce la siguiente
-información de los microprocesadores: marca (Intel, AMD, NVidia, etc), línea (Xeon, Core i7, Opteron, Atom,
-
-Centrino, etc.), cantidad de cores o núcleos de procesamiento (1, 2, 4, 8), velocidad del reloj (medida en Ghz) y
-tamaño en nanómetros (nm) de los transistores (14, 22, 32, 45, etc.). La información de los microprocesadores
-se lee de forma consecutiva por marca de procesador y la lectura finaliza al ingresar un procesador con 0 cores
-(que no debe procesarse). Se pide implementar un programa que lea información de los microprocesadores
-de la empresa importadora e informe:
+información de los productos que mantiene actualmente en stock. Para ello, se conoce la siguiente información de
+los microprocesadores: marca (Intel, AMD, NVidia, etc), línea (Xeon, Core i7, Opteron, Atom, Centrino, etc.),
+cantidad de cores o núcleos de procesamiento (1, 2, 4, 8), velocidad del reloj (medida en Ghz) y tamaño en
+nanómetros (nm) de los transistores (14, 22, 32, 45, etc.). La información de los microprocesadores se lee de
+forma consecutiva por marca de procesador y la lectura finaliza al ingresar un procesador con 0 cores (que no
+debe procesarse). Se pide implementar un programa que lea información de los microprocesadores de la empresa
+importadora e informe:
 ● Marca y línea de todos los procesadores de más de 2 cores con transistores de a lo sumo 22 nm.
 ● Las dos marcas con mayor cantidad de procesadores con transistores de 14 nm.
-● Cantidad de procesadores multicore (de más de un core) de Intel o AMD, cuyos relojes alcancen
-velocidades de al menos 2 Ghz 
+● Cantidad de procesadores multicore (de más de un core) de Intel o AMD, cuyos relojes alcancen velocidades de
+al menos 2 Ghz.
 
 program EJ6P3;
 type 
@@ -78,8 +77,8 @@ var
 begin 
 	max1:=-1; max2:= -1; m1:= ''; m2:= ''; multi:= 0;
 	leer(r);
+	marcaA:= ''; lineaA:= ''; cant:= 0;
 	while (r.cant_nucleos <> 0) do begin 
-		marcaA:= ''; lineaA:= ''; cant:= 0;
 		marcaACT:= r.marca;
 		while (marcaACT = r.marca) do begin 
 			proceso(r,multi,marcaA,lineaA);
@@ -87,9 +86,8 @@ begin
 			mayor(r,cant,max1,max2,m1,m2);
 			leer(r);
 		end;
-		writeln('Marca y línea de todos los procesadores de más de 2 cores con transistores de a lo sumo 22 nm: ', 'MARCA: ', marcaA , 'LINEA: ', lineaA);
 	end;
-	writeln('Las dos marcas con mayor cantidad de procesadores con transistores de 14 nm: ', m1 , m2);
-	
+	writeln('Las dos marcas con mayor cantidad de procesadores con transistores de 14 nm: ', m1 ,'y ', m2);
+	writeln('Marca y línea de todos los procesadores de más de 2 cores con transistores de a lo sumo 22 nm: ' , ' MARCA: ', marcaA , ' LINEA: ', lineaA);
 	writeln('Cantidad de procesadores multicore (de más de un core) de Intel o AMD, cuyos relojes alcancen velocidades de al menos 2 Ghz: ', multi);
 end.
