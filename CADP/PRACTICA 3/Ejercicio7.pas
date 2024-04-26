c62afc43f548e426aa4e7bea25399801c0e9ac54
Hernan <83614099+Hernan404@users.noreply.github.com>
2024-04-26 10:13:00 -0300

    Update Ejercicio7.pas

--- CADP/PRACTICA 3/Ejercicio7.pas
+++ CADP/PRACTICA 3/Ejercicio7.pas
@@ -1,60 +1,66 @@
 program EJ7P3;
-
-type
-	universidad = record
+type 
+	data = record 
 		nombre:string;
 		uni:string;
-		cantInves:integer;
-		cantbecarios:integer;
-		
-		cantcentros:integer;
-		
-		mayorcantidad:string;
-		
-		centrosmenores:string;
-		
-		max:integer;
-		min:integer;
-	end;
-
-procedure leer (var u:universidad);
-begin
-	readln(u.nombre);
-	u.cantcentros:= u.cantcentros +1;
-	readln(u.uni);
-	readln(u.cantInves);
-	readln(u.cantbecarios);
+		cant_in:integer;
+		cant_be:integer;
 end;
 
-procedure mayor (var u:universidad);
+procedure leer(var r:data);
 begin
-	if (u.cantInves > u.max) then
-		u.max:= u.cantInves;
-		u.mayorcantidad:= u.uni;
+	with r do begin 
+		writeln('NOMBRE');
+		readln(nombre);
+		writeln('UNIVERSIDAD');
+		readln(uni);
+		writeln('CANTIDAD DE INVESTIGADORES');
+		readln(cant_in);
+		writeln('CANTIDAD DE BECARIOS');
+		readln(cant_be);
+	end;
+end; 
+
+procedure mayor (r:data; var max:integer; var u1:string);
+begin
+	if (r.cant_in > max) then 
+		max:= r.cant_in;
+		u1:= r.uni;
 end;
 
-procedure menor (var u:universidad);
-begin
-	if (u.cantbecarios < u.min) then begin
-		u.min:=u.cantbecarios;
-		u.centrosmenores:= u.nombre;
+procedure menor (r:data; var min1,min2:integer; var c1,c2:string);
+begin 
+	if (r.cant_be < min1) then begin 
+		min2:= min1;
+		c2:= c1;
+		min1:= r.cant_be;
+		c1:= r.nombre;
+	end
+	else if (min1 < min2) then begin 
+		min2:= r.cant_be;
+		c2:= c1;
 	end;
 end;
 
-
-
-var
-	u:universidad;
-begin
-	u.max:= -1;
-	u.min:= 9999;
-	while (u.cantInves <> 0) do begin
-		leer(u);
-		mayor(u);
-		menor(u);
-	end;
+var 
+	r:data; 
+	max,min1,min2,centrostotal:integer;
+	u1,c1,c2,uniACT:string;
+begin 
+	max:= -1; min1:= 9999; min2:= 9999; c1:= ''; c2:= ''; u1:= '';
 	
-writeln('cantidad total de centros: ', u.cantcentros);
-writeln('mayor: ',u.mayorcantidad);
-writeln('menor: ', u.centrosmenores);
+	leer(r);
+	while (r.cant_in <> 0) do begin 
+		uniACT:= r.uni;
+		centrostotal:= 0;
+		while (uniACT = r.uni) do begin 
+			centrostotal:= centrostotal +1;
+			mayor(r,max,u1);
+			menor(r,min1,min2,c1,c2);
+			leer(r);
+		end;
+		writeln('CENTROS DE LA UNIVERSIDAD ',uniACT, ':', centrostotal);
+	end;
+	writeln('Universidad con mayor cantidad de investigadores en sus centros: ', u1);
+	writeln('Los dos centros con menor cantidad de becarios: ', c1 ,'y', c2);
 end.
