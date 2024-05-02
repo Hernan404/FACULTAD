a6a7a3c2c696c19b3c2a20241eb7e6d77302b4d8
Hernan <83614099+Hernan404@users.noreply.github.com>
2024-05-02 17:57:53 -0300

    Update Ejercicio11.pas

--- CADP/PRACTICA 4/Ejercicio11.pas
+++ CADP/PRACTICA 4/Ejercicio11.pas
@@ -1,94 +1,70 @@
 program EJ11P4P1;
-
 const 
-	dimf = 2;
-type
-	
+	dimf = 200;
+type 
+	data = record 
+		titulo:string;
+		autor:string;
+		cantMG:integer;
+		cantCLicks:integer;
+		cantComents:integer;
+	end;
 
-    fotos = record
-        titulo:string;
-        autor:string;
-        cantMegusta:integer;
-        cantClick:integer;
-        cantComents:integer;
-    end;
-    
-    rango= 1..dimf;
-    vector = array [rango] of fotos;
+	vector = array [1..dimf] of data;
 
-procedure leer_datos (var f:fotos);
-begin
-	writeln('titulo');
-    readln(f.titulo);
-    writeln('autor');
-    readln(f.autor);
-    writeln('cantidad de megusta');
-    readln(f.cantmegusta);
-    writeln('cantidad de clicks');
-    readln(f.cantClick);
-    writeln('cantidad de comentarios');
-    readln(f.cantComents);
-end;
-
-procedure masvista (var v:vector; var f:fotos; var max:integer; var fotomasvista:string);
-var
-	i:rango;
-begin
-	for i:= 1 to dimf do begin
-		if (v[i].cantClick > f.cantClick) then 
-			f:= v[i];
-    end;
-end;
-
-procedure megustas (v:vector; f:fotos);
-var
-	i:rango;
-begin
+procedure inivector(var v:vector);
+var 
+	i:integer;
+begin 
 	for i:=1 to dimf do begin
-		if (v[i].autor = 'Art Valenday') then
-			f:= v[i];
+		with r do begin  
+			writeln('TITULO');
+			readln(titulo);
+			writeln('AUTOR'));
+			readln(autor);
+			writeln('CANTIDAD DE MEGUSTAS');
+			readln(cantMG);
+			writeln('CANTIDAD DE CLICKS');
+			readln(cantCLicks);
+			writeln('CANTIDAD DE COMENTARIOS');
+			readln(cantComents);
+		end;
 	end;
 end;
 
-procedure cantcomentarios (v:vector; f:fotos);
-var
-	i:rango;
-begin
-	for i:=1 to dimf do begin
-		f:= v[i];
-    end;
+procedure mayor(v:vector; var max:integer; var puntoA:string);
+
+begin 
+	if (v[i].likes > max) then begin 
+		max:= v[i].likes;
+		puntoA:= v[i].titulo;
+	end;
 end;
 
-procedure leectura (var f:fotos; var v:vector; max:integer; var fotomasvista:string; var cantidad_comentarios:integer);
-var
-    i:rango;
-begin
-    for i:= 1 to dimf do begin
-        leer_datos (f);
-        masvista(v,f,max,fotomasvista);
-        megustas(v,f);
-        cantcomentarios(v,f);
-    end;
-end;
-
-
+procedure procesardatos(v:vector);
 var 
-    max:integer;
-    fotomasvista:string;
+	r:data;	max:integer; puntoB:integer; puntoC:integer;
+	puntoA:string;
+begin 
+	max:= -1; puntoB:= 0; puntoA:='';
+	
+	for i:=1 to dimf do begin 
+			mayor(max,v,puntoA);
+			if (v[i].autor = 'Art Vandenlay') then 
+				puntoB:= puntoB + v[i].cantMG;
+		inivector(v);
+	end;
 
-   { megustasfoto:integer;}
-
-    cantidad_comentarios:integer;
-
-    f:fotos;
-    v:vector;
-begin
-    max:= -1;
-    leectura(f,v,max,fotomasvista,cantidad_comentarios);
-    
-
-    writeln('foto mas vista: ', f.cantClick);
-    writeln('cantidad de me gusta: ', f.cantMegusta);
-    writeln('cantidad de comentarios: ', f.cantComents);
-
+	writeln('titulo de la foto mas vista: ', puntoA);
+	writeln('cantidad total de megustas del art vandelay', putnoB);
+	for i:=1 to dimf do 
+		writeln('cantidad de comentarios recibidos para la foto ',v[i].titulo ,  ' : ',v[i].cantComents);
+end;
+var 
+	r:data;
+	v:vector;
+	i:integer;
+begin 
+	inivector(v);
+	procesardatos(v);
 end.
