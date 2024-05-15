bf1e395cea513a146cf8fbd77e9bb64732df0a24
Hernan <83614099+Hernan404@users.noreply.github.com>
2024-05-15 11:40:43 -0300

    Update Ejercicio9.pas

--- CADP/PRACTICA 6/Ejercicio9.pas
+++ CADP/PRACTICA 6/Ejercicio9.pas
@@ -1,111 +1,105 @@
+{realize un modulo EstáOrdenada: recibe la lista como parámetro y retorna true si la misma se encuentra ordenada, o
+false en caso contrario}
 
+program JugamosConListas;
+type
+	lista = ^nodo;
+	nodo = record
+	num : integer;
+	sig : lista;
+end;
 
-function buscar (l:lista; x:integer):boolean;
-var 
-	cumple:true;
+procedure armarNodo(var L: lista; v: integer);
+var
+	aux : lista;
+begin
+	new(aux);
+	aux^.num := v;
+	aux^.sig := L;
+	L := aux;
+end;
+
+procedure puntoD(L:lista; numD:integer);
 begin 
-	cumple:= false;
-	while (L <> nil) and (not cumple) and (x > L^.dato) do begin 
-		if (x = L^.dato) then 
-			cumple:= true;
-		else
-			cumple:= false;
+	readln(numD);
+	while (L <> nil) do begin 
+		L^.num:= L^.num + numD; 
+		L:= L^.sig;
 	end;
-	buscar:= encontre;
-end;	
+end;
 
-procedure borrarElemento (var pri:lista; nom:string; var exito: boolean);
+procedure imprimir (L:lista; num:integer);
+begin 
+	while (L <> nil) then  
+		writeln(L^.num);
+		L:= L^.sig;
+end; 
+
+function estaOrdenada(L:lista):boolean;
 var 
-	ant,act:lista;
-begin
-	exito := false;
-	act := pri;
-	
-	while (act <> nil) and (act^.datos.nom <> nom) do begin 
-		ant:= act;
-		act:= act^.sig
+	x:integer;
+	aux:lista;
+begin 
+	aux:= L^.sig;
+	if (aux <> nil) then begin 
+		if(aux^.num > L^.num) then begin 
+			x:= 0;
+			while (L <> nil) and (L^.num > x) do begin 
+				x:= L^.num;
+				L:= L^.sig;
+			end;
+		end
+	else begin 
+		x:= 32767;
+		while (L <> nil) and (L^.num < x) do begin 
+			x:= L^.num;
+			L:= L^.sig;
+		end;
 	end;
-	
-	if (act <> nil) then begin 
-		exito:= true;
-		if (act = pri) then 
-			pri := act^.sig;
-		else
-			ant^.sig:= act^.sig;
-		dispose (act);
+	estaOrdenada:= (L = nil);
 end;
 
-
-
-procedure sublista(var L: lista; A, B: integer; var resultado: lista);
-var
-  nodo: lista;
-begin
-  nodo := L; // Creamos un nodo auxiliar para recorrer la lista
-
-  while (nodo <> nil) do
+procedure crearSublista (var L: lista; n: integer);
+  var
+    nue: lista;
   begin
-    if (nodo^.datos > A) and (nodo^.datos < B) then
-      agregarfinal(resultado, nodo^.datos); // Llamamos al módulo agregarfinal para agregar el elemento a la lista resultado
-
-    nodo := nodo^.sig; // Avanzamos al siguiente nodo de la lista
+    New(nue);
+    nue^.num:= n;
+    nue^.sig:= L;
+    L:= nue;
   end;
-end;
 
-procedure agregarfinal(var L: lista; num: integer);
-var
-  nue: lista;
-  nodo: lista;
-begin
-  new(nue);
-  nue^.datos := num;
-  nue^.sig := nil;
-
-  if L = nil then
+// ================ INCISO C,D y E
+procedure sublista (L: lista; var listaSub: lista);
+  var
+    a,b: integer;
   begin
-    L := nue;
-  end
-  else
-  begin
-    nodo := L;
-
-    while (nodo^.sig <> nil) do
-      nodo := nodo^.sig;
-
-    nodo^.sig := nue;
+    write ('Numero A: '); 
+	readln(a);
+    write ('Numero B: '); 
+	readln(b);
+    while (L <> nil) do
+      begin
+        if (L^.num > a) and (L^.num < b) then
+          crearSublista(listaSub,L^.num);
+        L:= L^.sig;
+      end;
   end;
-end;
-
-	
-procedure Sublista(L: lista; A, B: integer; var nuevalista: lista);
-begin
-  while (L <> nil) and (L^.num <= B) do
-  begin
-    if (L^.num > A) then
-    begin
-      armarNodo(nuevalista, L^.num);
-      Writeln('Cumple ', L^.num);
-    end;
-    L := L^.sig;
-  end;
-  Writeln('Cosa');
-  imprimirNodo(nuevalista);
-end;
 
 var
-	A,B:integer;
 	pri : lista;
 	valor : integer;
 begin
-	A:23;
-	B:50;
 	pri := nil;
-	writeln(‘Ingrese un numero’);
+	writeln('Ingrese un numero');
 	read(valor);
-	while (valor <> 0) then begin
+	while (valor <> 0) do begin
 		armarNodo(pri, valor);
-		writeln(‘Ingrese un numero’);
+		writeln('Ingrese un numero');
 		read(valor);
 	end;
-. . . { imprimir lista }
+	puntoD(pri,valor);
+	
+	imprimir(pri,valor);
+	
 end.
