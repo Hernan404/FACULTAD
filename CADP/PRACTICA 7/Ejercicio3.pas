d781a59d58154be9f48464778d14948f3499a0cc
Hernan <83614099+Hernan404@users.noreply.github.com>
2024-05-21 11:02:41 -0300

    Update Ejercicio3.pas

--- CADP/PRACTICA 7/Ejercicio3.pas
+++ CADP/PRACTICA 7/Ejercicio3.pas
@@ -1,131 +1,96 @@
 program EJ3P7;
-
 type 
-    viaje = record 
-        numero:integer;
-        codigo:integer;
-        odireccion:string;
-        dDireccion:string;
-        km:real;
-    end;
+	viaje = record 
+		numero:integer;
+		codigo:integer;
+		dir_origen:integer;
+		dir_destino:integer;
+		km:real;
+	end;
 
-    lista1 = ^nodo;
-    nodo = record 
-        data:viaje;
-        sig:lista1;
-    end;
+	lista = ^nodo;
+	nodo = record 
+		data:viaje;
+		sig:lista;
+	end;
 
-    lista2 = ^nodo;
-    nodo = record 
-        data:viaje;
-        sig:lista2;
-    end;
-
-procedure armarnodo1 (var L:lista; d:data);
+procedure armarlistaOrdenada (var pri:lista; r:viaje);
 var 
-    aux:lista;
-begin 
-    new(aux);
-    aux^.data:= d;
-    aux^.sig:= L;
-    L:= aux;
+    ant, nue, act: lista;
+begin
+    new (nue);
+    nue^.datos := per;
+    act := pri;
+    ant := pri;
+    {Recorro mientras no se termine la lista y no encuentro la posición correcta}
+    while (act<>NIL) and (act^.data.codigo < r.codigo) do //De menor a mayor
+    begin
+        ant := act;
+        act := act^.sig ;
+    end;
+    if (ant = act)  then 
+        pri := nue   {el dato va al principio}
+    else  
+        ant^.sig  := nue; {va entre otros dos o al final}
+    nue^.sig := act ;
 end;
 
-procedure armarnodo2 (var L:lista; d:data);
-var 
-    aux:lista;
+procedure puntoA(var max1,max2,c1,c2:integer; r:viaje);
 begin 
-    new(aux);
-    aux^.data:= d;
-    aux^.sig:= L;
-    L:= aux;
+	if (r.km > max1) then begin 
+		max2:= max1;
+		c2:= c1;
+		max1:= r.km;
+		c1:= r.codigo
+	end
+	else if (max1 > max2) then begin 
+		max2:= r.km;
+		c2:= r.codigo;
+	end;
 end; 
 
-procedure leer (var d:data);
-begin 
-    with d do begin 
-    readln(numero);
-    readln(codigo);
-    readln(odireccion);
-    readln(dDireccion);
-    readln(km);
-    end;
-end;
-procedure maskm(var d:data; var max1,max2:integer);
-begin 
-    if (L^.d.km > max1) then begin 
-        max2:= max1;
-        max1:= codmax1;
-        KLmax1:= L^.d.km;
-        codmax1:= L^.d.codigo;
-    end;
-    else 
-        if (d.km > max2) then begin  
-            max2:= d.codigo;
-            codmax2:= L^.d.codigo;
-        end;
-end;
 
-procedure listaViajes (L:lista; d:data);
-begin 
-    if(d.km > 5) then 
-        armarnodo2(L,d);
-        cant:= cant +1;
-    end;
-end;
-
-procedure InsertarOrdenado (var ListaNueva:lista; V:viaje);
+procedure armarlista2 (var L2:lista; r:viaje);
 var 
-    ant,nue,act:lista;
-begin
-    new(nue);
-    nue^.datos:=V;
-    act:=ListaNueva;
-    ant:=ListaNueva;
-    while (act <>nil) and (act^.datos.num_viaje<V.num_viaje) do
-    begin
-        ant:=act;
-        act:=act^.sig;
-    end;
-    if(ant = act)then
-    begin
-        ListaNueva:=nue;
-    end
-    else
-    begin
-        ant^.sig:=nue;
-    end;    
-end;
-
-begin
-    ListaNueva:=nil;
-    codmax1:=-1;
-    codmax2:=-1;
-    klmax1:=-1;
-    klmax2:=-1;
-    while L <> Nil do
-    begin
-        cantKM:=0;
-        act:=L^.datos.cod_auto;
-        while (L<>Nil) and (act=L^.datos.cod_auto) do
-        begin
-            if(L^.datos.klrecorrido>5)then
-            begin
-                InsertarOrdenado(ListaNueva,L^.datos);
-            end;
-            cantKM:=cantKM+L^.datos.klrecorrido;
-            L:=L^.sig;  
-        end;
-        actualizarMaximos(L,codmax1,codmax2,klmax1,klmax2);
-    end;
-end;
-
-procedure cargardatos (L:lista; d:data);
+	aux:lista;
 begin 
-    repeat 
-        leer(d);
-        armarnodo1(L,d);
-        maskm(d,max1,max2);
-        listaviajes(L,d);
-    until cant = 6;
+	new(aux);
+	aux^.data:= r;
+	aux^.sig:= L2;
+	L2:= sig;
+end; 
+
+procedure procesardatos(L:lista);
+var 
+	L2:lista;
+	max1,max2,c1,c2:integer;
+begin 
+	max1:= -1;
+	max2:= -1;
+	c1:= 0;
+	c2:= 0;
+
+	while (L <> nil) do begin 
+
+		viajeACT:= L^.data.codigo;
+		cantviajes:= 0;
+
+		while (L <> nil) and (viajeACT = L^.data.codigo) do begin 
+			cantviajes:= cantviajes +1;
+			puntoA(max1,max2,c1,c2,L^.data);
+
+			if (L^.data.km > 5) then 
+				armarlista2(L2,L^.data);
+		
+		L:= L^.sig;
+		end;
+	end;
+end;
+
+var 
+	L:lista;
+begin 
+	L:= nil;
+	cargardatos(L);
+	procesardatos(L);
 end;
