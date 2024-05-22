5dec516e6a89c97817df39a15adff8c942b93252
Hernan <83614099+Hernan404@users.noreply.github.com>
2024-05-22 17:46:32 -0300

    Update Ejercicio10.pas

--- CADP/PRACTICA 7/Ejercicio10.pas
+++ CADP/PRACTICA 7/Ejercicio10.pas
@@ -158,3 +158,275 @@ begin
     procesardatos(L);
 end.
 
+{Una compañía de venta de insumos agrícolas desea procesar la información de las empresas a las que les
+provee sus productos. De cada empresa se conoce su código, nombre, si es estatal o privada, nombre de la
+ciudad donde está radicada y los cultivos que realiza (a lo sumo 20). Para cada cultivo de la empresa se
+registra: tipo de cultivo (trigo, maíz, soja, girasol, etc.), cantidad de hectáreas dedicadas y la cantidad de
+meses que lleva el ciclo de cultivo.
+a. Realizar un programa que lea la información de las empresas y sus cultivos. Dicha información se
+ingresa hasta que llegue una empresa con código -1 (la cual no debe procesarse). Para cada empresa
+se leen todos sus cultivos, hasta que se ingrese un cultivo con 0 hectáreas (que no debe procesarse).
+Una vez leída y almacenada la información, calcular e informar:
+b. Nombres de las empresas radicadas en “San Miguel del Monte” que cultivan trigo y cuyo código de
+empresa posee al menos dos ceros.
+c. La cantidad de hectáreas dedicadas al cultivo de soja y qué porcentaje representa con respecto al
+total de hectáreas.
+d. La empresa que dedica más tiempo al cultivo de maíz
+e. Realizar un módulo que incremente en un mes los tiempos de cultivos de girasol de menos de 5
+hectáreas de todas las empresas que no son estatales.}
+
+program EJ10P7;
+type 
+    empresa = reco rd 
+        codigo:integer;
+        nombre:string;
+        estado:string;
+        ciudad:string;
+        cultivos:tipos;
+    end;
+
+    tipos = record 
+        tipo:string;
+        cant_hectareas:integer;
+        cant_meses:integer;
+    end;
+
+    lista = ^nodo 
+    nodo = record 
+        data:empresa;
+        sig:lista;
+    end;
+
+    vector = array [1..20] of tipos;
+
+procedure inivecor (var v:vector);
+var 
+    i:integer;
+begin 
+    for i:=1 to diml
+end;
+
+procedure cargardatos(L:lista; L2:lista_cultivos);
+var 
+    r:empresa;
+    c:tipos;
+    diml:integer;
+begin
+    diml:= 0;
+
+    leer_empresa(r);
+    while(r.codigo <> -1) do begin 
+        armarlista_empresa(L,r);
+        leer_cultivos(c);
+
+        while (c.cant_hectareas <> 0) and (diml < 20) do begin 
+            // CARGAR EN VECTOR
+            diml:= diml +1;
+            leer_cultivos(c);
+        end;
+
+        leer_empresa(r);
+    end;
+end;
+
+
+
+procedure puntoB();
+
+procedure puntoC();
+
+procedure puntoD();
+
+
+
+
+procedure puntoE ();
+var 
+
+begin 
+end;
+
+
+procedure procesardatos(L:lista; L2:lista_cultivos);
+var 
+    v:vector;
+begin 
+
+    while (L <> nil) do begin 
+
+        for i:=1 to diml do begin 
+        //PUNTO B
+        if(L^.data.ciudad = 'san miguel') and (v[i].tipo = 'trigo') and (cumple0(L^.data)) then 
+            writeln(L^.data.nombre);
+
+        //PUNTO C
+        total_hec:= total_hec + v[i].cant_hectareas;
+
+        if (v[i].tipo = 'soja') then begin 
+            cantC:= cantC +1;
+            puntoC:= cantC/total_hec;
+        end;
+
+        if (v[i].tipo = 'maiz') then begin 
+            if (v[i].cant_meses > max) then begin  
+                max:= v[i].cant_meses;
+                p1:= L^.data.nombre;
+            end;
+        end;
+
+        if (v[i].tipo = 'girasol') and (v[i].cant_hectareas < 5) and (L^.data.estado = 'privada')
+
+
+    end;
+end;
+
+
+
+
+var 
+    L:= lista;
+begin 
+    L:= nil;
+    cargardatos(L);
+    procesardatos(L);
+end.
+
+
+
+
+
+
+
+
+
+program EJ10P7;
+type 
+    tipos = record 
+        tipo:string;
+        cant_hectareas:integer;
+        cant_meses:integer;
+    end;
+
+    empresa = record 
+        codigo:integer;
+        nombre:string;
+        estado:string;
+        ciudad:string;
+        cultivos: array [1..20] of tipos;
+        cant_cultivos: integer; // para llevar el registro del número de cultivos
+    end;
+
+    lista = ^nodo; 
+    nodo = record 
+        data:empresa;
+        sig:lista;
+    end;
+
+procedure leer_empresa(var e:empresa);
+begin
+    // Aquí deberías implementar la lógica para leer los datos de la empresa
+end;
+
+procedure armarlista_empresa(var L:lista; e:empresa);
+begin
+    // Aquí deberías implementar la lógica para agregar una empresa a la lista
+end;
+
+procedure leer_cultivos(var c:tipos);
+begin
+    // Aquí deberías implementar la lógica para leer los datos de los cultivos
+end;
+
+function cumple0(e:empresa): boolean;
+begin
+    // Aquí deberías implementar la lógica para verificar si un código de empresa tiene al menos dos ceros
+end;
+
+procedure incrementar_meses_girasol(var e:empresa);
+begin
+    // Aquí deberías implementar la lógica para incrementar en un mes los cultivos de girasol con menos de 5 hectáreas
+end;
+
+procedure cargardatos(var L:lista);
+var 
+    r:empresa;
+begin
+    leer_empresa(r);
+    while (r.codigo <> -1) do 
+    begin
+        armarlista_empresa(L, r);
+        leer_empresa(r);
+    end;
+end;
+
+procedure puntoB(L:lista);
+var 
+    temp:lista;
+begin
+    temp := L;
+    while temp <> nil do
+    begin
+        // Aquí deberías implementar la lógica para el punto B
+        temp := temp^.sig;
+    end;
+end;
+
+procedure puntoC(L:lista);
+var 
+    temp:lista;
+    total_hec, cant_soja: integer;
+begin
+    temp := L;
+    total_hec := 0;
+    cant_soja := 0;
+
+    while temp <> nil do
+    begin
+        // Aquí deberías implementar la lógica para el punto C
+        temp := temp^.sig;
+    end;
+
+    // Aquí deberías imprimir el resultado
+end;
+
+procedure puntoD(L:lista);
+var 
+    temp:lista;
+    max_meses: integer;
+    empresa_maiz: string;
+begin
+    temp := L;
+    max_meses := 0;
+
+    while temp <> nil do
+    begin
+        // Aquí deberías implementar la lógica para el punto D
+        temp := temp^.sig;
+    end;
+
+    // Aquí deberías imprimir el resultado
+end;
+
+procedure procesardatos(L:lista);
+var 
+    temp:lista;
+begin
+    temp := L;
+    while temp <> nil do
+    begin
+        // Aquí deberías implementar la lógica general para procesar los datos
+        puntoB(temp);
+        puntoC(temp);
+        puntoD(temp);
+        incrementar_meses_girasol(temp^.data);
+        temp := temp^.sig;
+    end;
+end;
+
+var 
+    L: lista;
+begin 
+    L:= nil;
+    cargardatos(L);
+    procesardatos(L);
+end.
