01289cbb50b1bffc813d3ce9f18aaf345809b03d
Hernan <83614099+Hernan404@users.noreply.github.com>
2024-06-03 10:19:01 -0300

    Create REPASO_PARCIAL_PRACTICO.pas

---
+++ CADP/REPASO PARCIALES VIEJOS/REPASO_PARCIAL_PRACTICO.pas
@@ -0,0 +1,19 @@
+{PARCIAL PRACTICA.
+un cine posee la lista de peliculas que proyectara durante el mes de febrero
+de cada pelicula se tiene codigo de pelicula, nombre de pelicula, codigo genero (1..8)
+y puntaje promedio otorgado por las criticas. dicha estructura no posee ningun orden
+
+se pide:
+A. actualizar (en la lista que se dispone) el puntaje promedio otorgado por las criticas
+para ello se debe leer desde teclado las criticas que han realizado criticos de cine. de 
+cada critica se lee DNI del critico, apellido y nombre del critico, codigo de pelicula y 
+puntaje otorgado. la lectura finaliza cuando se lee el codigo de pelicula -1 y la informacion
+viene ordenada por codigo de pelicula
+
+B. informar el codigo de genero que mas puntaje obtuvo entre todas las criticas
+
+C. informar el apellido y nombre de aquellos criticos que posean la misma cantidad de digitos
+pares e impares en su DNI
+
+D. realizar un modulo que elimine de la lista que se dispone una pelicula cuyo codigo
+se recibe como parametro (el mismo puede no existir)}
