40492c4246e7fb68c4273f2f50341a838a6ce056
Hernan <83614099+Hernan404@users.noreply.github.com>
2024-06-05 16:31:20 -0300

    Update Ejercicio13.pas

--- CADP/PRACTICA 7/Ejercicio13.pas
+++ CADP/PRACTICA 7/Ejercicio13.pas
@@ -1,4 +1,4 @@
-12. El centro de deportes Fortaco’s quiere procesar la información de los 4 tipos de suscripciones que 
+{12. El centro de deportes Fortaco’s quiere procesar la información de los 4 tipos de suscripciones que 
 ofrece: 1)Musculación, 2)Spinning, 3)Cross Fit, 4)Todas las clases. Para ello, el centro dispone de una tabla 
 con información sobre el costo mensual de cada tipo de suscripción.
 Realizar un programa que lea y almacene la información de los clientes del centro. De cada cliente se 
@@ -9,7 +9,7 @@ Una vez almacenados todos los datos, procesar la estructura de datos generada, c
 - La ganancia total de Fortaco’s
 - Las 2 suscripciones con más clientes.
 - Genere una lista con nombre y DNI de los clientes de más de 40 años que están suscritos a CrossFit
-o a Todas las clases. Esta lista debe estar ordenada por DNI
+o a Todas las clases. Esta lista debe estar ordenada por DNI}
 
 
 program EJ13P7;
