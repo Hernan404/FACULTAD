a18f92401adf74af6190ea07276a5b6352cba675
Hernan <83614099+Hernan404@users.noreply.github.com>
2024-06-06 17:05:21 -0300

    Update [FALTA CORREJIR] Ejercicio11.pas

--- CADP/PRACTICA 7/[FALTA CORREJIR] Ejercicio11.pas
+++ CADP/PRACTICA 7/[FALTA CORREJIR] Ejercicio11.pas
@@ -11,6 +11,7 @@ type
 		lugar:string;
 		cant_personas:integer;
 		costo:real;
+		capacidad:integer;
 	end;
 
 	ventas = record 
@@ -19,8 +20,8 @@ type
 		DNI:integer;
 		cant_entradas:integer;
 	end;
-	vector = array [eventos] of integer;
-	vector2 = array [eventos] of evento;
+	vector = array [eventos] of evento;
+	vpuntoA = array [eventos] of real;
 
 	lista = ^nodo;
 	nodo = record 
@@ -29,28 +30,70 @@ type
 	end;
 	
 	
-procedure inivector (var v:vector; var v2:vector2);
+procedure cargar_evento (var vp2:vpuntoA);
+var 
+	i:integer;
 begin 
-
+	for i:=1 to eventos do begin 
+		readln(vp2[i].nombre);
+		readln(vp2[i].tipo);
+		readln(vp2[i].lugar);
+		readln(vp2[i].cant_personas);
+		readln(vp2[i].costo);
+	end;
 end;
 
-procedure puntoA (v2:vector2; min1,min2:real; a1,a2,e1,e2:string);
+
+
+procedure leer_ventas (var r:ventas);
+begin 
+	with r do begin 
+		readln(codigo);
+		readln(DNI);
+		readln(numero);
+		readln(cant_entradas);
+	end;
+end;
+
+
+procedure crearLista (var L: Lista; r:ventas);
+  var
+    nue: Lista;
+  begin
+    New(nue);
+    nue^.data:= r;
+    nue^.sig:= L;
+    L:= nue;
+  end;
+
+procedure cargardatos(var L:lista);
+var 
+	i:integer;
+begin 
+	leer_ventas(r);
+	while (r.codigo <> -1) do begin 
+		armarlista(L,r);
+		leer_ventas(r);
+	end;
+end;
+
+procedure puntoA (vp2:vpuntoA; min1,min2:real; a1,a2,e1,e2:string);
 var 
 	i:integer;
 begin 
 	for i:=1 to 100 do begin 
-		if (v2[i].costo < min1) then begin 
+		if (vp2[i].costo < min1) then begin 
 			min2:= min1;
 			a2:= a1;
 			e2:= e1;
-			min1:= v2[i].costo;
-			a1:= v2[i].nombre;
-			e2:= v2[i].lugar;
+			min1:= vp2[i].costo;
+			a1:= vp2[i].nombre;
+			e2:= vp2[i].lugar;
 		end
 		else if (min1 < min2) then begin 
-			min2:= v2[i].costo;
-			a2:= v2[i].nombre;
-			e2:= v2[i].lugar;
+			min2:= vp2[i].costo;
+			a2:= vp2[i].nombre;
+			e2:= vp2[i].lugar;
 		end;
 	end;
 end;
@@ -74,40 +117,49 @@ begin
 end;
 	
 
-procedure procesardatos(L:lista);
+procedure procesardatos(L:lista; v:vector);
 var 
-	min1,min2:real; a1,a2,e1,e2:string; cantB:integer; maximoC:boolean;
-	v:vector; v2:vector2;
+	min1,min2:real; a1,a2,e1,e2:string; cantB:integer; 
+	vp2:vpuntoA; 
 	i:integer;
+	cantC:integer;
+	costo:real;
 begin 
-	cantB:= 0;
-	a1:= ''; a2:= ''; e1:= ''; e2:= ''; min1:= 999; min2:= 999; maximoC:= false; 
-	inivector(v,v2);
-	while (L <> nil) do begin 
+	cantB:= 0; costo:= 0;
+	cantC:= 0;
+	for i:=1 to eventos do 
+		vp2[i]:= 0; 
 		
-		puntoA(v2, min1,min2,a1,a2,e1,e2);
+	a1:= ''; a2:= ''; e1:= ''; e2:= ''; min1:= 999; min2:= 999;  
+	while (L <> nil) do begin 
+	
+		//punto A
+		costo:= v[L^.data.numero].costo * L^.data.cant_entradas;
+		vp2[L^.data.numero]:= vp2[L^.data.numero] + costo; 
+		
 		//punto B
-		if (v2[L^.data.numero].tipo = 3) and (cumplePar(L^.data.DNI)) then 
+		if (v[L^.data.numero].tipo = 3) and (cumplePar(L^.data.DNI)) then 
 			cantB:= cantB + L^.data.cant_entradas;
-			
-		if (L^.data.numero = 50) and (v2[50].cant_personas < L^.data.numero) then 
-			maximoC:= true;
+		
+		//punto C
+		if (L^.data.numero = 50) then  
+		cantC:= cantC +1;
 	end;
-		for i:=1 to 100 do 
-			writeln('punto A', a1,a2,e1,e2);
-			
+		puntoA(vp2, min1,min2,a1,a2,e1,e2);
 		writeln('punto B', cantB);
 		
-		if (maximoC) then
-			writeln('puntoC cumple ');
+		if (v[50].capacidad < cantC) then 
+			writeln('no alcanzo')
+		else 
+			writeln('supero');
 end;
 
 var 
-	L:lista;
+	L:lista; vp2:vpuntoA; r:ventas; v:vector;
 begin 
 	L:= nil;
-	cargareventos(L); 
-	cargarventas(L);
-	procesardatos(L);
+	cargar_evento(v);
+	cargardatos(L);
+	procesardatos(L,v);
 end.
 
