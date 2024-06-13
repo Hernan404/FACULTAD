7bb3170efb962e8bd45be53be22740f241b2a931
Hernan <83614099+Hernan404@users.noreply.github.com>
2024-06-13 15:15:22 -0300

    Update TEMA_2_.pas

--- CADP/REPASO PARCIALES VIEJOS/PARCIAL 2024/TEMA_2_.pas
+++ CADP/REPASO PARCIALES VIEJOS/PARCIAL 2024/TEMA_2_.pas
@@ -1,4 +1,5 @@
-{un fabricante de dispositivos electronicos desea procesar informacion de los repuestos que compro.
+{TEMA 2: 
+un fabricante de dispositivos electronicos desea procesar informacion de los repuestos que compro.
 de cada repuesto conoce su codigo, precio, codigo de marca (entre 1 y 130) y nombre del pais del que proviene.
 el fabricante dispone de una estructura de datos con la informacion de los repuestos, ordenados por nombre de
 pais.
@@ -9,119 +10,136 @@ Realizar un progrma que:
 		1. la cantidad de paises a los que se le compro mas de 100 repuestos
 		2. para cada marca, nombre de la marca y el precio del producto mas barato
 		3. la cantidad de repuestos que no poseen ningun cero en su codigo.}
+	
+
 
 
 program TEMA2;
-const
-  marcas = 130;
+const 
+	marcas = 130;
+type 
+	rango_marcas = 1..marcas;
+	
+	repuesto = record 
+		codigo:integer;
+		precio:real;
+		cod_marca:rango_marcas;
+		nombre:string;
+	end;
 
-type
-  rango_marcas = 1..marcas;
-  
-  repuesto = record
-    codigo: integer;
-    precio: real;
-    cod_marca: rango_marcas;
-    nombre_pais: string;
-  end;
-  
-  lista = ^nodo;
-  nodo = record
-    data: repuesto;
-    sig: lista;
-  end;
-  
-  vMarcas = array[rango_marcas] of string;
-  vCant = array[rango_marcas] of integer;
-  vPrecios = array[rango_marcas] of real;
+	trabaja = record 
+		codigo:string;
+		nombre:string;
+	end;
+	
+	lista = ^nodo;
+	nodo = record 
+		data:repuesto;
+		sig:lista;
+	end;
+	
+	vMarcas = array [rango_marcas] of trabaja;
+	vPrecios = array [rango_marcas] of real;
 
-var
-  L: lista;
+procedure cargardatos(L); // se dispone
 
-procedure cargardatos(var L: lista); // Se dispone
-begin
-  // Implementación no provista
+procedure inivector(vc:vCant);
+var 
+	i:integer;
+begin 
+	for i:=1 to marcas do begin 
+		vc[i]:= 0;
+	end;
 end;
 
-procedure leer(var vm: vMarcas);
-var
-  i: integer;
-begin
-  for i := 1 to marcas do
-  begin
-    readln(vm[i]);
-  end;
+
+procedure leer(var cod:integer; var nom:string;);
+begin 
+	readln(cod);
+	readln(nom);
 end;
 
-procedure inivector(var vc: vCant);
-var
-  i: integer;
-begin
-  for i := 1 to marcas do
-    vc[i] := 0;
+procedure cargarmarca(var L:lista); 
+var 
+	vm:vMarcas;
+	cod,i:integer;
+	nom:string;
+begin 
+	for i:=1 to marcas do begin 
+		leer(cod,nom);
+		vm[cod]:= nom;  // guardo en [cod] y no en [i] porque no tiene que estar ordenado
+	end;
 end;
 
-procedure cargarmarca(var L: lista; var vm: vMarcas);
-begin
-  leer(vm);
+procedure inivector(vp:vprecio);
+var 
+	i:integer;
+begin 
+	for i:=1 to marcas do 
+		vp[i]:= 999;
 end;
 
-function cumple3(cod: integer): boolean;
-var
-  cero: boolean;
-begin
-  cero := false;
-  while (cod <> 0) do
-  begin
-    if ((cod mod 10) = 0) then
-      cero := true;
-    cod := cod div 10;
-  end;
-  cumple3 := not cero;
+procedure punto2(var min:real; valorNUE:real);
+begin 
+	if (valorNUE < min) then 
+		min:= valorNUE;
+end;	
+
+procedure cumple3(cod:integer):boolean;
+var 
+	cero:boolean;
+begin 
+	cero:= true;
+	while (cod <> 0) and (cero = true) do begin 
+		if ((cod mod 10) = 0) then
+			cero:= false;
+		cod:= cod div 10;
+	end;
+	cumple3:= (not cero);
 end;
 
-procedure procesardatos(L: lista; var vm: vMarcas);
-var
-  vc: vCant;
-  vp: vPrecios;
-  cant3, i, paises100: integer;
-  marcaActual: rango_marcas;
-begin
-  cant3 := 0;
-  paises100 := 0;
-  inivector(vc);
-  for i := 1 to marcas do
-    vp[i] := MaxInt;
+procedure procesardatos(L:lista);
+var 
+	vm:vMarcas; vc:vCant; vp:vprecios; cant3:integer;
+	cant_repuesto:integer;
 
-  while (L <> nil) do
-  begin
-    marcaActual := L^.data.cod_marca;
-    vc[marcaActual] := vc[marcaActual] + 1;
+begin 
+	cant3:= 0; max:= -1; p1:= 0;
+	inivector(vc);
+	while (L <> nil) do begin 
+		paisACT:= L^.data.codigo;
+		cant_repuesto:= 0;
+		while (L <> nil) and (L^.data.codigo = paisACT) do begin 
 
-    if (vc[marcaActual] = 101) then
-      paises100 := paises100 + 1;
+			cant_repuesto:= cant_repuesto +1;
 
-    if (L^.data.precio < vp[marcaActual]) then
-    begin
-      vp[marcaActual] := L^.data.precio;
-    end;
+			punto2(vp[L^.data.cod_marca], L^.data.precio);
 
-    if cumple3(L^.data.codigo) then
-      cant3 := cant3 + 1;
+	  	 	if (cumple3(L^.data.codigo)) then 
+				cant3:= cant3 +1;
 
-    L := L^.sig;
-  end;
+		L:= L^.sig;
+		end;
+		if (cant_repuesto > 100) then 
+			cant1:= cant1 +1;
+	end;
+	writeln('punto 1 ', punto1);
+	
+	writeln('punto 3 ', punto3);	
 
-  writeln('Cantidad de países con más de 100 repuestos:', paises100);
-  for i := 1 to marcas do
-    if vp[i] < MaxInt then
-      writeln('Marca ', vm[i], ' - Precio más barato: ', vp[i]:0:2);
-  writeln('Cantidad de repuestos sin ceros en su código:', cant3);
+	for i:=1 to marcas do 
+		writeln('punto 2 ', vm[i], vp[i]) // vm actua como posicion, vp actua como el puntero
 end;
 
-begin
-  L := nil;
-  cargardatos(L); // Se dispone
-  cargarmarca(L, vMarcas);
-  procesardatos(L, vMarcas);
+
+var 
+	L:lista;
+begin 
+	L:= nil;
+	cargardatos(L); // se dispone;
+	cargardatosmarca(L);
+	procesardatos(L);
 end.
+
+
+
