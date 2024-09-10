da4aa2ea5f792d62261e0d82d3e88d2aba4f0cda
Hernan <83614099+Hernan404@users.noreply.github.com>
2024-09-10 09:48:20 -0300

    Create Ejercicio6.pas

---
+++ CADP/PRACTICA 4/Ejercicio6.pas
@@ -0,0 +1,56 @@
+program EJ6P4;
+const 
+	dimf = 100;
+type 
+	vector = array [1..dimf] of integer;
+ 
+ 
+
+procedure cargar(var v:vector; num:integer);
+var 
+	i:integer;
+begin 
+	for i:=1 to dimf do begin 
+		readln(num);
+		v[i]:= num;
+	end;
+end;
+ 
+procedure proceso (var v:vector);
+ var 
+	i:integer;
+	num:integer;
+	posMAX,posMIN:integer;
+	aux:integer;
+begin 
+	max:= -1; min:= 999; posMAX:= 0; posMIN:= 0;
+	
+	
+	for i:=1 to dimf do begin 
+		if (v[i] > max) then begin 
+			max:= v[i];
+			posMAX:= i;
+		end;
+	
+		if (v[i] < min) then begin 
+			min:= v[i];
+			posMIN:= i;
+		end;
+	end;
+	
+	for i:=1 to dimf do begin 
+		v[aux]:= v[posMIN];
+		v[posMIN]:= v[posMAX];
+		v[posMAX]:= v[aux];
+	end;
+	writeln('el elemento máximo', max 'que se encontraba en la posición' posMAX 'fue intercambiado con el elemento mínimo' min 'que se encontraba en la posición' posMIN
+end;
+   
+ var 
+	v:vector; num:integer;
+begin 
+	readln(num);
+	cargar(v,num);
+	proceso(v);
+    
+end.    
