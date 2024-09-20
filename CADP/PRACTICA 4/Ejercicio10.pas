38bec325afe6f9fb1a31b723101f7b0ca0bfc665
Hernan <83614099+Hernan404@users.noreply.github.com>
2024-09-20 08:46:14 -0300

    Update Ejercicio10.pas

--- CADP/PRACTICA 4/Ejercicio10.pas
+++ CADP/PRACTICA 4/Ejercicio10.pas
@@ -1,115 +1,61 @@
-program Vectores;
- const
- dimF=300;
- aumento=1.15;
- 
- type
-    str20=string[20];
-    
-    vector =  Array [1..dimF] of real;
-
-//_________________________________________________________________________________
-procedure leer_salarios(var salarioF:vector;var dimL:integer);
-var
-    i:integer;
-begin
-    i:=1;
-    readln(salarioF[i]);
-    while ((i<dimF) and (salarioF[i]<>0)) do
-    begin
-        i:=i+1;
-        ReadLn(salarioF[i]);
-    end; 
-    dimL:=i;
-end;
-
-//_________________________________________________________________________________
-procedure aumentar_salarios(var salarios:vector;dimL:integer);
-var 
-    i:integer;
-begin
-    for i:=1 to dimL do
-        begin
-            salarios[i]:=salarios[i]*aumento;
-        end;
-end;
-//_________________________________________________________________________________
-procedure Promedio_empresa(salarios:vector;dimL:integer;var promedio:real);
-var
-    suma_total:real;
-    i:integer;
-begin
-    suma_total:=0;
-    for i:=1 to dimL do
-    begin
-        suma_total:=suma_total+salarios[i];
-    end;
-    promedio:=suma_total/dimL;
-
-end;
-//_________________________________________________________________________________
-var
-    dimL:integer;
-    salarios:vector;
-    salario_Indi:real;
-    promedio:real;
-begin
-    promedio:=0;
-    dimL:=1;
-    leer_salarios(salarios,dimL);
-    WriteLn('A___________________');
-    aumentar_salarios(salarios,dimL);
-    WriteLn('B___________________');
-    Promedio_empresa(salarios,dimL,promedio);
-    WriteLn('El promedio es: ', promedio:4:3);
-end.
-
-
-
-
-====================================
-
+{Realizar un programa que lea y almacene el salario de los empleados de una empresa de turismo (a lo
+sumo 300 empleados). La carga finaliza cuando se lea el salario 0 (que no debe procesarse) o cuando
+se completa el vector. Una vez finalizada la carga de datos se pide:
+a. Realizar un módulo que incremente el salario de cada empleado en un 15%. Para ello, implementar
+un módulo que reciba como parámetro un valor real X, el vector de valores reales y su dimensión
+lógica y retorne el mismo vector en el cual cada elemento fue multiplicado por el valor X.
+b. Realizar un módulo que muestre en pantalla el sueldo promedio de los empleados de la empresa}
 
 program EJ10P4;
 const 
-	dimf = 300;
+  dimf = 300;
 type 
-	
-	vector = array [1..dimf] of real;
-	
-procedure cargar(var v:vector; salario:real; var diml:integer);
+  vector = array [1..dimf] of real;
+
+procedure cargar(var v: vector; var diml: integer);
 var 
-	i:integer;
-begin 
-	while (diml < dimf) and (salario <> 0) do begin 
-		for i:=1 to dimf do begin 
-			diml:= diml +1;
-			v[diml]:= salario;
-			readln(salario);
-		end;
-	end;
+  salario: real;
+begin
+  diml := 0; // Inicializamos la dimensión lógica
+  writeln('Ingrese el salario del empleado (0 para finalizar):');
+  readln(salario);
+  
+  while (diml < dimf) and (salario <> 0) do begin
+    diml := diml + 1;
+    v[diml] := salario;
+    writeln('Ingrese el salario del empleado (0 para finalizar):');
+    readln(salario);
+  end;
 end;
 
-procedure puntoA(salario:real; var v:vector; diml:integer);
+procedure incrementarSalarios(var v: vector; diml: integer; porcentaje: real);
 var 
-	i:integer; aumento:real;
-begin 
-	aumento:= 0;
-	for i:=1 to diml do begin 
-		aumento:= v[i] * 15
-		v[i]:= v[i] + aumento;
-	end;
+  i: integer;
+begin
+  for i := 1 to diml do begin
+    v[i] := v[i] * (1 + porcentaje / 100); // Aumentar el salario en el porcentaje indicado
+  end;
+end;
+
+procedure mostrarPromedio(v: vector; diml: integer);
+var 
+  i: integer;
+  suma: real;
+begin
+  suma := 0;
+
+  for i := 1 to diml do begin
+    suma := suma + v[i]; // Sumar los salarios
+  end;
+  
+  writeln('El salario promedio es: ', suma / diml:0:2)
 end;
 
 var 
-	diml:integer;
-	v:vector; salario:real;
-begin 
-	readln(salario);
-	cargar(v,salario,diml);
-	puntoA(salario,v,diml);
-	
-	writeln(aumento)
-	for i:=1 to diml do 
-		writeln(diml/dimf);
+  v: vector;
+  diml: integer;
+begin
+  cargar(v, diml);
+  incrementarSalarios(v, diml, 15); // Aumentar salarios en un 15%
+  mostrarPromedio(v, diml);         // Mostrar el promedio de salarios
 end.
