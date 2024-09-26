e6e07d01b86d33199de205505ea4d7184e331382
Hernan <83614099+Hernan404@users.noreply.github.com>
2024-09-26 15:24:04 -0300

    Create [SIN HACER] Ejercicio19.pas

---
+++ CADP/PRACTICA 7/[SIN HACER] Ejercicio19.pas
@@ -0,0 +1,19 @@
+{19. Un comercio necesita procesar la información de los pedidos realizados. El comercio dispone de la
+información de sus 2000 productos. De cada producto se almacena nombre, marca y número de rubro (1
+a 15) al que pertenece.
+Implementar un programa que:
+a) Invoque a un módulo que lea la información de los pedidos y la procese. De cada pedido se lee número
+de pedido, fecha y todos los productos solicitados en el pedido. De cada producto solicitado se lee código
+de producto (1 a 2000) y cantidad comprada. La lectura de la información de los pedidos finaliza con el
+número -999 y, para cada pedido, la lectura de sus productos solicitados finaliza con el código de
+producto -999. El módulo debe:
+i- Retornar, en una estructura adecuada, la cantidad de productos solicitados para cada rubro.
+ii- Retornar, en una estructura adecuada, los números de pedidos junto a su cantidad de
+productos solicitados, para los pedidos que tengan más de 10 productos solicitados.
+iii- Informar la cantidad total de productos solicitados al comercio.
+b) Lea un valor, e invoque a un módulo que reciba el valor leído y la estructura retornada en a) que
+corresponda para imprimir los números de pedidos que tengan la misma cantidad de dígitos pares e
+impares y cuyo cantidad total de productos pedidos sea menor al valor recibido.
+c) Invoque a un módulo que reciba la estructura retornada en a) que corresponda para retornar el
+número de rubro con mayor cantidad de productos solicitados. El programa debe informar el valor
+retornado por el módulo.}
