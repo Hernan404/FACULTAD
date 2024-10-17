20c546ade803c7d09ecd69d1efd68e281e65085d
Hernan <83614099+Hernan404@users.noreply.github.com>
2024-10-17 13:51:25 -0300

    Update [RESUELTO] Ejercicio15.pas

--- CADP/PRACTICA 7/[RESUELTO] Ejercicio15.pas
+++ CADP/PRACTICA 7/[RESUELTO] Ejercicio15.pas
@@ -18,25 +18,6 @@ menos de 5 unidades.       if (L^.data.unidades >= v[i])
 c) Un módulo que imprima los códigos de los pedidos no satisfechos donde el DNI del cliente
 tenga la misma cantidad de dígitos pares e impares.}
 
-{Una fábrica de pastas elabora diversos productos y quiere procesar los pedidos que recibe. La fábrica
-ofrece 15 tipos de productos (por ej.: 1. tallarines cinta media; 2. ravioles de verdura; 3. ravioles de ricota;
-etc.). Para cada tipo de producto se conoce su nombre, unidades disponibles y el precio unitario del
-producto. La fábrica dispone de dicha información.
-Implementar un programa con:
-
-a) Un módulo que atienda los pedidos. De cada pedido se lee código de pedido, DNI cliente, tipo
-de producto (de 1 a 15) y cantidad de unidades pedidas. La lectura de los pedidos finaliza con el
-DNI igual a -1. El módulo debe:
-    i- Informar el código del pedido y el monto a pagar, de los pedidos que se pueden
-    satisfacer.
-    ii- Retornar una lista con los pedidos no satisfechos.
-    iii- Informar el monto total recaudado con los pedidos que se pudieron satisfacer.
-
-b) Un módulo que retorne la cantidad de productos cuyas unidades disponibles quedaron en
-menos de 5 unidades.       if (L^.data.unidades >= v[i]) 
-
-c) Un módulo que imprima los códigos de los pedidos no satisfechos donde el DNI del cliente
-tenga la misma cantidad de dígitos pares e impares.}
 
 
 program EJ15P7;
