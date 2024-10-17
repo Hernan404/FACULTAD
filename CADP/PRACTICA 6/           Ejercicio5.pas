0466da1d889dac280fabe648303357650100e409
Hernan <83614099+Hernan404@users.noreply.github.com>
2024-10-17 13:58:53 -0300

    Update            Ejercicio5.pas

--- CADP/PRACTICA 6/           Ejercicio5.pas
+++ CADP/PRACTICA 6/           Ejercicio5.pas
@@ -1,134 +1,141 @@
-Realizar un programa que lea y almacene la información de productos de un supermercado. De cada
+{Realizar un programa que lea y almacene la información de productos de un supermercado. De cada
 producto se lee: código, descripción, stock actual, stock mínimo y precio. La lectura finaliza cuando se
 ingresa el código -1, que no debe procesarse. Una vez leída y almacenada toda la información, calcular
 e informar:
 a. Porcentaje de productos con stock actual por debajo de su stock mínimo.
 b. Descripción de aquellos productos con código compuesto por al menos tres dígitos pares.
-c. Código de los dos productos más económicos.
+c. Código de los dos productos más económicos.}
 
 program EJ5P6;
 type 
-	datos = record 
-		codigo:integer;
-		descripcion:string;
-		stockACT:integer;
-		stockMIN:integer;
-		precio:real;
-	end;
-	
-	lista = ^nodo; 
-	nodo = record 
-		data:datos;
-		sig:lista;
-	end;
-procedure leer(var r:datos);
+  datos = record 
+    codigo: integer;
+    descripcion: string;
+    stockACT: integer;
+    stockMIN: integer;
+    precio: real;
+  end;
+  
+  lista = ^nodo; 
+  nodo = record 
+    data: datos;
+    sig: lista;
+  end;
+
+procedure leer(var r: datos);
 begin 
-	with r do begin 
-		writeln('CODIGO');
-		readln(codigo);
-		if (codigo <> -1) then begin
-			writeln('DESCRIPCION');
-			readln(descripcion);
-			writeln('STOCK ACTUAL');
-			readln(stockACT);
-			writeln('STOCK MINIMO');
-			readln(stockMIN);
-			writeln('PRECIO');
-			readln(precio);
-		end;
-	end;
+  with r do begin 
+    writeln('Ingrese el CODIGO:');
+    readln(codigo);
+    if (codigo <> -1) then begin
+      writeln('Ingrese la DESCRIPCION:');
+      readln(descripcion);
+      writeln('Ingrese el STOCK ACTUAL:');
+      readln(stockACT);
+      writeln('Ingrese el STOCK MINIMO:');
+      readln(stockMIN);
+      writeln('Ingrese el PRECIO:');
+      readln(precio);
+    end;
+  end;
 end;
 
-procedure armarnodo(var L:lista; r:datos);
+procedure armarnodo(var L: lista; r: datos);
 var 
-	aux:lista;
+  aux: lista;
 begin 
-	new(aux);
-	aux^.data:= r;
-	aux^.sig:= L;
-	L:= aux;
+  new(aux);
+  aux^.data := r;
+  aux^.sig := L;
+  L := aux;
 end;
 
-procedure cargardatos(L:lista);
+procedure cargardatos(var L: lista);
 var 
-	r:datos;
+  r: datos;
 begin 
-	leer(r);
-	while (r.codigo <> -1) do begin 
-		armarnodo(L,r);
-		leer(r);
-	end;
+  leer(r);
+  while (r.codigo <> -1) do begin 
+    armarnodo(L, r);
+    leer(r);
+  end;
 end;
 
-function pares (num:integer):boolean;
+function pares(num: integer): boolean;
 var 
-	par,impar:integer;
+  par: integer;
 begin 
-	par:= 0;
-	impar:= 0;
-	
-	while (num > 0) do begin 
-		num:= num mod 10;
-		
-		if (num mod 2 = 0) then begin 
-			par:= par +1;
-		end
-		else begin  
-			impar:= impar +1;
-		end;
-	num:= num div 10;
-	end;
-	pares:= (par = 3);
+  par := 0;
+  
+  while (num > 0) do begin 
+    if ((num mod 10) mod 2 = 0) then 
+      par := par + 1;
+    num := num div 10;
+  end;
+  pares := (par >= 3);  // Verificamos si hay al menos 3 dígitos pares
 end;
 
-procedure minimos (r:datos; var min1,min2: real; var cod1,cod2:integer);
+procedure minimos(r: datos; var min1, min2: real; var cod1, cod2: integer);
 begin 
-	if (r.precio < min1) then begin 
-		min2:= min1;
-		cod2:= cod1;
-		min1:= r.precio;
-		cod1:= r.codigo;
-	end
-	else if (min1 < min2) then begin 
-			min2:= r.precio;
-			cod2:= r.codigo;
-	end;
+  if (r.precio < min1) then begin 
+    min2 := min1;
+    cod2 := cod1;
+    min1 := r.precio;
+    cod1 := r.codigo;
+  end
+  else if (r.precio < min2) then begin 
+    min2 := r.precio;
+    cod2 := r.codigo;
+  end;
 end;
-procedure procesardatos(L:lista);
+
+procedure procesardatos(L: lista);
 var 
-	r:datos; porcenA:real; cod1,cod2:integer; min1,min2:real; cantproductos:integer;
+  porcenA: real; 
+  cod1, cod2: integer; 
+  min1, min2: real; 
+  cantproductos, cantBajoStock: integer;
 begin 
-	porcenA:= 0;
-	cod1:= 0;
-	cod2:= 0;
-	min1:= 999;
-	min2:= 999;
-	cantproductos:= 0;
-	
-	while (L <> nil) do begin 
-		cantproductos:= cantproductos +1;
-		
-		//A
-		if (L^.data.stockACT < L^.data.stockMIN) then 
-			porcenA:= porcenA + 1;
-		
-		//B
-		if (pares(L^.data.codigo)) then 
-			writeln('Descripción de producto con código compuesto por al menos tres dígitos pares. ',L^.data.descripcion);
-		
-		//C
-		minimos(r,min1,min2,cod1,cod2);
-	L:=L^.sig;
-	end;
-	
-	writeln('Porcentaje de productos con stock actual por debajo de su stock mínimo: ', (porcenA/cantproductos) * 100:2:0);
-	writeln('Código de los dos productos más económicos: ', cod1,' y ', cod2);
+  porcenA := 0;
+  cod1 := 0;
+  cod2 := 0;
+  min1 := 999999;  // Valores iniciales altos para asegurar la comparación
+  min2 := 999999;
+  cantproductos := 0;
+  cantBajoStock := 0;
+  
+  while (L <> nil) do begin 
+    cantproductos := cantproductos + 1;
+    
+    // A - Contar productos con stock bajo
+    if (L^.data.stockACT < L^.data.stockMIN) then 
+      cantBajoStock := cantBajoStock + 1;
+    
+    // B - Descripción de productos con código con al menos 3 dígitos pares
+    if (pares(L^.data.codigo)) then 
+      writeln('Descripción de producto con código compuesto por al menos tres dígitos pares: ', L^.data.descripcion);
+    
+    // C - Buscar los dos productos más económicos
+    minimos(L^.data, min1, min2, cod1, cod2);
+    
+    L := L^.sig;
+  end;
+  
+  // Calcular el porcentaje
+  if (cantproductos > 0) then
+    porcenA := (cantBajoStock / cantproductos) * 100
+  else
+    porcenA := 0;
+  
+  writeln('Porcentaje de productos con stock actual por debajo de su stock mínimo: ', porcenA:0:2, '%');
+  writeln('Código de los dos productos más económicos: ', cod1, ' y ', cod2);
 end;
 
 var 
-	L:lista;
+  L: lista;
 begin 
-	L:= nil;
-	cargardatos(L);
-	procesardatos(L);
+  L := nil;
+  cargardatos(L);
+  procesardatos(L);
 end.
+
