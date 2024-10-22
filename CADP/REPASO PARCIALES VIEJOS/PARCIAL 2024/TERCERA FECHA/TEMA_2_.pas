98e5ad780feacb8fbba6761e13c6a5de150c4668
Hernan <83614099+Hernan404@users.noreply.github.com>
2024-10-22 12:32:33 -0300

    Update TEMA_2_.pas

--- CADP/REPASO PARCIALES VIEJOS/PARCIAL 2024/TERCERA FECHA/TEMA_2_.pas
+++ CADP/REPASO PARCIALES VIEJOS/PARCIAL 2024/TERCERA FECHA/TEMA_2_.pas
@@ -1,1 +1,14 @@
-
+{La FIFA desea procesar informacion del torneo "Copa del Mundo" entre paises. para ello,
+dispone de una estructura de datos con la informacion de cada pais que ha participado
+alguna vez del evento. de cada pais se conoce el nombre del pais, cantidad total de copas obtenidas,
+la cantidad de participaciones en mundiales e informacion de dichas participaciones (a lo sumo 22)
+de cada participacion se conoce el año y en que instancia finalizo (1: fase de grupos, 2: octavos de final, 3: cuartos de final, 4:semifinal; 5:final)
+realizar un programa
+a. lea la informacion de un nuevo pais y la incorpore en la estructura de datos que se dispone del nuevo pais 
+se lee el nombre del pais, la cnatidad de copas obtenidas, la cantidad de participaciones y la infomracion de cada 
+participacion (año y en que instancia finalizo en dicha participacion) 
+b. una vez cargado el nuevo pais, procese los datos de todos los paises, e informe: 
+	1. para cada pais ,a la cantida de participaciones realizadas en años anteirores al 1998 en las que 
+	superaros los octavos de final 
+	2. los nombres de los dos paises que llegaron mas veces a instancias de final 
+	3. el promedio de participantes en copas del mundo entre todos los paises}
