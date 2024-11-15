55c04c67f1cdf7f217382604a1ff443b89a14176
Hernan <83614099+Hernan404@users.noreply.github.com>
2024-11-15 10:43:52 -0300

    Create TEMA_1_.pas

---
+++ CADP/REPASO PARCIALES VIEJOS/PARCIAL 2024/SEGUNDO SEMESTRE/TEMA_1_.pas
@@ -0,0 +1,12 @@
+{una fabrica de automoviles necesita un programa para administrar las piezas de los diferentes modelos de automoviles
+que fabrica. de cada pieza, se conoce: codigo de pieza, descripcion, cantidad en stock, costo de la pieza, codigo de 
+modelo al que pertenece(1 al 10), stock minimo. la lectura finaliza al leer el codigo de pieza 9999 el cual debe procesarse 
+
+A. leer y almacenar la informacion de las piezaas en una estructura de datos adecuada. la informacion debe quedar 
+almacenada en el mismo orden que fue leida 
+
+B. a partir de la estructura generada en A) calcular e informar 
+    1. los 2 codigos de modelo d automoviles mas baratos de fabricar 
+    2. el promedio de piezas con codigo de modelo 3 y cuyo codigo de pieza comienza en 9 
+    3. cual es el codigo de peiza en que la fabrica debera gastar mas dinero para que su stock actual supere
+    en una unidad a su stock minimo.}
