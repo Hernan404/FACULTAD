f66febdfdd26e36545c9af235ecebc0f0aee83ee
Hernan <83614099+Hernan404@users.noreply.github.com>
2024-11-19 12:39:43 -0300

    Update    [REVISAR] Ejercicio18.pas

--- CADP/PRACTICA 7/   [REVISAR] Ejercicio18.pas
+++ CADP/PRACTICA 7/   [REVISAR] Ejercicio18.pas
@@ -9,22 +9,28 @@ b) Lea un nombre de localidad e invoque a un módulo que recibe dicho nombre y l
 ventas, e informe los dos meses de menor facturación de esa localidad. Se debe implementar una función
 que reciba las ventas de un mes y una localidad y devuelva el monto total facturado de la localidad.}
 
-program EJ18P7;
+program EJ18P7l;
+const 
+    mes = 12;
 type 
+    rango_mes = 1..12;
+
     venta = record 
         nombre: string;
         monto: real;
-    end;
+        mes: rango_mes; // Se incluye el mes explícitamente
+    end; 
 
     lista = ^nodo;
-    nodo = record 
+    nodo = record
         data: venta;
         sig: lista;
-    end;
+    end; 
 
-    vector = array [1..12] of real;
+    vector = array [rango_mes] of real;
 
-procedure armarlista(var L: lista; r: venta);
+// Agrega un elemento al inicio de la lista
+procedure agregaradelante(var L: lista; r: venta);
 var 
     aux: lista;
 begin 
@@ -34,91 +40,83 @@ begin
     L := aux;
 end;
 
+// Carga los datos en una lista enlazada
 procedure cargardatos(var L: lista);
 var 
     r: venta;
     i: integer;
 begin 
-    for i := 1 to 12 do 
-    begin 
-        writeln('Ingrese ventas para el mes ', i);
+    for i := 1 to mes do begin
+        writeln('Ingresando datos del mes ', i);
         repeat
-            readln(r.nombre);
+            readln(r.nombre); 
             readln(r.monto);
-            if r.monto <> 0 then
-                armarlista(L, r);
-        until r.monto = 0;
+            if r.monto <> 0 then begin
+                r.mes := i;
+                agregaradelante(L, r);
+            end;
+        until r.monto = 0; // Finaliza la lectura del mes cuando el monto es 0
     end;
 end;
 
-function totalfacturado(L: lista; nom: string): real;
-var
-    total: real;
-begin
-    total := 0;
-    while (L <> nil) do
-    begin
-        if (L^.data.nombre = nom) then
-            total := total + L^.data.monto;
-        L := L^.sig;
-    end;
-    totalfacturado := total;
-end;
-
-procedure menorFacturacion(v: vector; var min1, min2: real; var p1, p2: integer);
-var
-    i: integer;
+// Calcula los dos meses de menor facturación
+procedure menorfacturacion(v: vector);
+var 
+    i:integer;
+    min1, min2, p1, p2: real;
 begin 
-    for i := 1 to 12 do 
-    begin 
-        if (v[i] < min1) then 
-        begin 
+    min1 := 999; min2 := 999; 
+    p1 := 0; p2 := 0;
+
+    for i := 1 to 12 do begin 
+        if v[i] < min1 then begin 
             min2 := min1;
             p2 := p1;
             min1 := v[i];
             p1 := i;
-        end 
-        else if (v[i] < min2) then 
-        begin 
+        end
+        else if v[i] < min2 then begin 
             min2 := v[i];
             p2 := i;
         end;
     end;
+    writeln('Mes con menor facturación: ', p1, ' (Facturación: ', min1:0:2, ')');
+    writeln('Segundo mes con menor facturación: ', p2, ' (Facturación: ', min2:0:2, ')');
 end;
 
-procedure procesardatos(nom: string; L: lista);
+// Procesa los datos para una localidad específica
+procedure procesardatos(L: lista; var v: vector; nombreB: string);
 var 
-    v: vector;
-    i, p1, p2: integer;
-    min1, min2: real;
-begin 
-    // Inicializar el vector de facturación a 0
-    for i := 1 to 12 do
+    i: integer;
+begin
+    // Inicializa el vector de facturación mensual
+    for i := 1 to 12 do 
         v[i] := 0;
 
-    // Calcular el total facturado para cada mes
-    for i := 1 to 12 do
-        v[i] := totalfacturado(L, nom);
+    // Recorre la lista y acumula montos para la localidad especificada
+    while L <> nil do begin 
+        if L^.data.nombre = nombreB then 
+            v[L^.data.mes] := v[L^.data.mes] + L^.data.monto;
+        L := L^.sig;
+    end;
 
-    min1 := 999999; 
-    min2 := 999999;
-    p1 := 0; 
-    p2 := 0; 
-
-    menorFacturacion(v, min1, min2, p1, p2);
-
-    writeln('Meses de menor facturación para la localidad ', nom, ':');
-    writeln('Mes ', p1, ' con facturación: ', min1:0:2);
-    writeln('Mes ', p2, ' con facturación: ', min2:0:2);
+    // Calcula los dos meses de menor facturación
+    menorfacturacion(v);
 end;
 
 var 
-    L: lista; 
-    nom: string;
+    L: lista;
+    v: vector;
+    nombreB: string;
 begin 
     L := nil;
+    writeln('Cargando datos de ventas...');
     cargardatos(L);
+
     writeln('Ingrese el nombre de la localidad a buscar:');
-    readln(nom);
-    procesardatos(nom, L);
+    readln(nombreB);
+
+    writeln('Procesando datos...');
+    procesardatos(L, v, nombreB);
 end.
+
