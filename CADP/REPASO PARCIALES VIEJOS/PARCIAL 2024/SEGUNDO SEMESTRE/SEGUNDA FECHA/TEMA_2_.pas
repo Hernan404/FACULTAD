41f523c0ce2ec53dc625c79cb8528023a19bcf1f
Hernan <83614099+Hernan404@users.noreply.github.com>
2024-11-29 11:33:39 -0300

    Update TEMA_2_.pas

--- CADP/REPASO PARCIALES VIEJOS/PARCIAL 2024/SEGUNDO SEMESTRE/SEGUNDA FECHA/TEMA_2_.pas
+++ CADP/REPASO PARCIALES VIEJOS/PARCIAL 2024/SEGUNDO SEMESTRE/SEGUNDA FECHA/TEMA_2_.pas
@@ -10,3 +10,128 @@ El módulo debe acceder al chico de la posición X y simular el pegado de las 6 
 si ya la tiene debe almacenarla en la estructura de las figuritas repetidas).
 C) Realizar el módulo que reciba la estructura generada en A y retorne el apellido, nombre del chico con menos figuritas en el álbum.
 D) Realizar un módulo que reciba la estructura generada en A, y retorne cuál es el código de selección que tiene más figuritas repetidas entre los 15 chicos.}
+
+program AlbumFiguritas;
+const
+  chicos = 15;
+  figuritas = 638;
+  seleccion = 32;
+type
+  rango_chicos = 1..chicos;
+  rango_figuritas = 1..figuritas;
+  rango_seleccion = 1..seleccion;
+
+  figurita = record
+    numero: rango_figuritas;
+    apeynom: string;
+    codigo: rango_seleccion;
+  end;
+
+  lista = ^nodo;
+  nodo = record
+    data: figurita;
+    sig: lista;
+  end;
+
+  chico = record
+    apellido: string;
+    nombre: string;
+    album: array[rango_figuritas] of boolean;
+    repetidas: lista;
+  end;
+
+  vchico = array[rango_chicos] of chico;
+  paquete = array[1..6] of figurita;
+  vseleccion = array[rango_seleccion] of integer;
+
+procedure cargardatos(var vc: vchico);
+var
+  i, j: integer;
+begin
+  for i := 1 to chicos do
+  begin
+    readln(vc[i].apellido);
+    readln(vc[i].nombre);
+    for j := 1 to figuritas do
+      vc[i].album[j] := false;
+    vc[i].repetidas := nil;
+  end;
+end;
+
+procedure agregarRepetida(var L: lista; f: figurita);
+var
+  nuevo: lista;
+begin
+  new(nuevo);
+  nuevo^.data := f;
+  nuevo^.sig := L;
+  L := nuevo;
+end;
+
+procedure pegarfigurita(posX: rango_chicos; p: paquete; var vc: vchico);
+var
+  i: integer;
+begin
+  for i := 1 to 6 do
+  begin
+    if not vc[posX].album[p[i].numero] then
+      vc[posX].album[p[i].numero] := true
+    else
+      agregarRepetida(vc[posX].repetidas, p[i]);
+  end;
+end;
+
+procedure menosfiguritas(vc: vchico; var apellido, nombre: string);
+var
+  i, j, min, cant: integer;
+begin
+  min := figuritas + 1; // Inicializamos con un valor mayor al máximo posible
+  for i := 1 to chicos do
+  begin
+    cant := 0;
+    for j := 1 to figuritas do
+      if vc[i].album[j] then
+        cant := cant + 1;
+    if cant < min then
+    begin
+      min := cant;
+      apellido := vc[i].apellido;
+      nombre := vc[i].nombre;
+    end;
+  end;
+end;
+
+procedure masfiguritas(vc: vchico; var maxCod: rango_seleccion);
+var
+  i: integer;
+  vsele: vseleccion;
+  aux: lista;
+begin
+  for i := 1 to seleccion do
+    vsele[i] := 0;
+  for i := 1 to chicos do
+  begin
+    aux := vc[i].repetidas;
+    while aux <> nil do
+    begin
+      vsele[aux^.data.codigo] := vsele[aux^.data.codigo] + 1;
+      aux := aux^.sig;
+    end;
+  end;
+  maxCod := 1;
+  for i := 2 to seleccion do
+    if vsele[i] > vsele[maxCod] then
+      maxCod := i;
+end;
+
+var
+  vc: vchico;
+  p: paquete;
+  apellido, nombre: string;
+  maxCod: rango_seleccion;
+begin
+  cargardatos(vc);
+  pegarfigurita(1, p, vc);
+  menosfiguritas(vc, apellido, nombre);
+  masfiguritas(vc, maxCod);
+end.
