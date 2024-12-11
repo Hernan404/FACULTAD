5d49d6da532c922ac7341f939f54c6401526f5a5
Hernan <83614099+Hernan404@users.noreply.github.com>
2024-12-11 10:37:14 -0300

    Update [REDICTADO] TEMA_1_.pas

--- CADP/PARCIALES/2023/TERCERA FECHA/[REDICTADO] TEMA_1_.pas
+++ CADP/PARCIALES/2023/TERCERA FECHA/[REDICTADO] TEMA_1_.pas
@@ -1,1 +1,11 @@
+{una tienda virtual necesita un programa para administrar la informacion de sus clientes. de cada cliente se lee 
+numero, DNI, apellido y nombre, fecha de nacimiento (dia,mes y año)
+nivel (1..5) y puntaje. la lectura finaliza cuando se lee el cliente con DNI 44555  
 
+A) generar una estructura que contenga numero, apellido y nombre con los clientes cuyos DNI 
+estan compuestos unicamente por digitos pares 
+
+B) informar los niveles con mayor y menor puntaje acumulado por los clientes nacidos antes del año 2000
+
+c) implementar un modulo que elimine, de la estructura generada, la informacion relacionada a un numero
+de cliente recibido por parametro. tener en cuenta que dicho numero puede no existir en la estructura}
