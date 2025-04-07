837e0f830ac8a21c83fedb7e9baea2b3357a041f
Hernan <83614099+Hernan404@users.noreply.github.com>
2025-04-07 12:48:58 -0300

    Update Ejercicio6.pas

--- CADP/PRACTICA 1/PARTE 2/Ejercicio6.pas
+++ CADP/PRACTICA 1/PARTE 2/Ejercicio6.pas
@@ -1,7 +1,7 @@
-Realizar un programa que lea información de 200 productos de un supermercado. De cada producto se lee
+{Realizar un programa que lea información de 200 productos de un supermercado. De cada producto se lee
 código y precio (cada código es un número entre 1 y 200). Informar en pantalla:
 ● Los códigos de los dos productos más baratos.
-● La cantidad de productos de más de 16 pesos con código par.
+● La cantidad de productos de más de 16 pesos con código par.}
 
 program EJ3P1P2;
 
