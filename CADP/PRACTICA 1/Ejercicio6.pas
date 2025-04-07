0e74555f9770e83a31618cca986cdab2c5941307
Hernan <83614099+Hernan404@users.noreply.github.com>
2025-04-07 12:20:02 -0300

    Update Ejercicio6.pas

--- CADP/PRACTICA 1/Ejercicio6.pas
+++ CADP/PRACTICA 1/Ejercicio6.pas
@@ -1,7 +1,7 @@
 {Realizar un programa que lea el número de legajo y el promedio de cada alumno de la facultad. La
 lectura finaliza cuando se ingresa el legajo -1, que no debe procesarse.}
 
-program EJ6P1;
+program EJ6|P1;
 
 var
 	legajo:integer;
@@ -19,27 +19,22 @@ begin
 
 	
 	while (legajo <> -1) do begin
-	
-	writeln('ingrese legajo');
-	readln(legajo);
-	
-	writeln('ingrese promedio');
-	readln(promedio);
-	
+		writeln('ingrese legajo');
+		readln(legajo);
+		writeln('ingrese promedio');
+		readln(promedio);
+		
 		cantalu:=cantalu+1;
-		
-		
+			
 		if (promedio > 6.5) then
 			cantalumayor:=+1;
-		
+			
 		if (promedio > 8.5) and (legajo < 2500)  then
 			cantaludest:= +1;
-		
+			
 	end;
 		
 	writeln('cantidad de alumnos ', cantalu);
 	writeln('cantidad de alumnos con promedio alto ', cantalumayor);
-	writeln('cantidad de alumnos destacados ', cantaludest);
-		
-		
+	writeln('cantidad de alumnos destacados ', cantaludest);	
 end.
