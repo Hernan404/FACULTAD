45e7c36b07aa9dd832928a7480a1786e8c6aea2b
Hernan <83614099+Hernan404@users.noreply.github.com>
2025-04-07 12:15:22 -0300

    Update Ejercicio4.pas

--- CADP/PRACTICA 1/Ejercicio4.pas
+++ CADP/PRACTICA 1/Ejercicio4.pas
@@ -1,18 +1,14 @@
 {Realizar un programa que lea un número real X. Luego, deberá leer números reales hasta que se
 ingrese uno cuyo valor sea exactamente el doble de X (el primer número leído)}
-
-program ej4p1;
-
+program EJ4|P1;
 var
-	num1:real;
-	doble:real;
-	
-
+    num1:real;
+    doble:real;
 begin
 	writeln('ingrese numero');
 	readln(num1);
 	doble:= num*2;
 	while (num1 <> doble) do begin
 		readln(num1);
-	end
+	end;
  end.
