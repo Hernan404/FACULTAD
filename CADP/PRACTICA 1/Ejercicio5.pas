843c1be468c0698f1158ec28f67655c522e6ddce
Hernan <83614099+Hernan404@users.noreply.github.com>
2025-04-07 12:17:15 -0300

    Update Ejercicio5.pas

--- CADP/PRACTICA 1/Ejercicio5.pas
+++ CADP/PRACTICA 1/Ejercicio5.pas
@@ -2,7 +2,7 @@
 reales. La lectura deberá finalizar al ingresar un valor que sea el doble de X, o al leer el décimo
 número, en cuyo caso deberá informarse “No se ha ingresado el doble de X”.}
 
-program ej5p1;
+program EJ5|P1;
 const 
 	limite = 10;
 
@@ -13,11 +13,9 @@ var
 
 begin
 	cant:= 0;
- 
 	writeln('ingrese numero');
 	readln(num1);
  	
-	
 	while (num <> doble) and (cant < limite) do begin
 		readln(num1);
 		cant:= cant +1;
@@ -25,6 +23,4 @@ begin
 
 	if (num1 = doble) and (cant < limite) then
 	    writeln('no se ha ingresado el numero doble');
-		
-
 end.
