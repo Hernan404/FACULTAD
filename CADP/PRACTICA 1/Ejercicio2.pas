5edbf9838cf9972cde5405167d3d8b8d33bb31e2
Hernan <83614099+Hernan404@users.noreply.github.com>
2025-04-07 12:12:59 -0300

    Update Ejercicio2.pas

--- CADP/PRACTICA 1/Ejercicio2.pas
+++ CADP/PRACTICA 1/Ejercicio2.pas
@@ -1,18 +1,20 @@
+{Realizar un programa que lea un número real e imprima su valor absoluto. El valor absoluto de un número
+X, se escribe |X| y se define como:
+|X| = X cuando X es mayor o igual a cero
+|X| = -X cuando X es menor a cero}
+
 program EJ2P1;
-
-
 var
 num1:real;
 absoluto:real;
 begin
-	writeln('ingrese un numero');
-	readln(num1);
-	if (num1 < 0) then 
-	    absoluto:= (num1 * -1);
-        else 
-            absoluto:= num;
-
- 	writeln('el valor absoluto es', absoluto);
+   writeln('ingrese un numero');
+   readln(num1);
+   if (num1 < 0) then 
+       absoluto:= (num1 * -1);
+   else 
+        absoluto:= num;
+ writeln('el valor absoluto es', absoluto);
 	
 
 end.
