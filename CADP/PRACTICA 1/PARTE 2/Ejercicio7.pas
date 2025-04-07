2fb6de58a68f368eeda859a436e3adca87f0e90c
Hernan <83614099+Hernan404@users.noreply.github.com>
2025-04-07 12:50:20 -0300

    Update Ejercicio7.pas

--- CADP/PRACTICA 1/PARTE 2/Ejercicio7.pas
+++ CADP/PRACTICA 1/PARTE 2/Ejercicio7.pas
@@ -1,8 +1,8 @@
-Realizar un programa que lea desde teclado información de autos de carrera. Para cada uno de los autos se
+{Realizar un programa que lea desde teclado información de autos de carrera. Para cada uno de los autos se
 lee el nombre del piloto y el tiempo total que le tomó finalizar la carrera. En la carrera participaron 100
 autos. Informar en pantalla:
 ● Los nombres de los dos pilotos que finalizaron en los dos primeros puestos.
-● Los nombres de los dos pilotos que finalizaron en los dos últimos puestos.
+● Los nombres de los dos pilotos que finalizaron en los dos últimos puestos.}
 
 program EJ87P1P2;
 var 
