59ca9f4737753ba131e00b95410fdd948420c68f
Hernan <83614099+Hernan404@users.noreply.github.com>
2025-04-07 12:11:08 -0300

    Update Ejercicio1.pas

--- CADP/PRACTICA 1/Ejercicio1.pas
+++ CADP/PRACTICA 1/Ejercicio1.pas
@@ -1,24 +1,21 @@
-program ej1p1;
+{Realizar un programa que lea 2 números enteros desde teclado e informe en pantalla cuál de los dos
+números es el mayor. Si son iguales debe informar en pantalla lo siguiente:
+Los números leídos son iguales}
 
+program ej1p1;
 var
 num1:char;
 num2:char;
-
 begin
-
 writeln('ingrese un numero');
 	readln(num1);
-
 writeln('ingrese el segundo numero');
 	readln(num2);
-
+ 
 if (num1 > num2) then
 	writeln( num1,' es mayor');
-	
 if (num1 < num2) then
 	writeln( num2,' es mayor')
-	
 else 
-	writeln('son iguales')
-	
+   writeln('son iguales')
 end.
