2a10d2e36e61e00fe7b4e85013ede193df1d6824
Hernan <83614099+Hernan404@users.noreply.github.com>
2025-04-09 11:25:50 -0300

    Update Ejercicio5.pas

--- CADP/PRACTICA 3/Ejercicio5.pas
+++ CADP/PRACTICA 3/Ejercicio5.pas
@@ -1,4 +1,11 @@
-program EJ3P3;
+{Realizar un programa que lea información de autos que están a la venta en una
+concesionaria. De cada auto se
+lee: marca, modelo y precio. La lectura finaliza cuando se ingresa la marca
+ “ZZZ” que no debe procesarse. La
+información se ingresa ordenada por marca. Se pide calcular e informar:
+a. El precio promedio por marca.
+b. Marca y modelo del auto más caro}
+program EJ5P3;
 type 
 	datos = record 
 		marca:string;
