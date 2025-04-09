19c12e78d40532c7dbcee51bc26173431645f1c8
Hernan <83614099+Hernan404@users.noreply.github.com>
2025-04-09 11:08:33 -0300

    Update Ejercicio4.pas

--- CADP/PRACTICA 3/Ejercicio4.pas
+++ CADP/PRACTICA 3/Ejercicio4.pas
@@ -1,5 +1,5 @@
 
-Una compañía de telefonía celular debe realizar la facturación mensual de sus 9300 clientes con planes de
+{Una compañía de telefonía celular debe realizar la facturación mensual de sus 9300 clientes con planes de
 consumo ilimitados (clientes que pagan por lo que consumen). Para cada cliente se conoce su código de
 cliente y cantidad de líneas a su nombre. De cada línea se tiene el número de teléfono, la cantidad de minutos
 consumidos y la cantidad de MB consumidos en el mes. Se pide implementar un programa que lea los datos
@@ -9,7 +9,7 @@ b. Realizar un módulo que reciba los datos de un cliente, lea la información d
 el módulo desarrollado en el inciso a y retorne la cantidad total de minutos y la cantidad total de MB a
 facturar del cliente.
 Nota: para realizar los cálculos tener en cuenta que cada minuto cuesta $3,40 y cada MB consumido cuesta
-$1,35.
+$1,35.}
 
 
 program EJ4P3;
