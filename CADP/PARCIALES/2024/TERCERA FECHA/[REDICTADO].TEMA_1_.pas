5b714454e69ba9ca63c169e63901ce521f40165e
Hernan <83614099+Hernan404@users.noreply.github.com>
2025-04-09 10:21:44 -0300

    Create [REDICTADO].TEMA_1_.pas

---
+++ CADP/PARCIALES/2024/TERCERA FECHA/[REDICTADO].TEMA_1_.pas
@@ -0,0 +1,17 @@
+{una empresa de alquiler de vehiculos dispone de una estructura donde tiene almaacenado toda la informacion de cada alquiler realizado durante el año 2023.
+
+de cada alquiler se conoce: patente de vehiculo, tipo de vehiculo (1..8), kms del automovil al retirarlo,
+kms del automovil al devolverlo, dni de la persona que lo alquilo, ciudad de retiro del vehiculo, ciudad de devolucion del vehiculo, fecha de retiro de vehiculo y cantidad de dias de alquiler. 
+esta informacion se encuentra ordenada por patente de manera ascendente.
+
+ademas la empresa dispone de una tabla con los montos de alquiler por dia de cada tipo de vehiculo 
+
+A partir de la informacion del os alquileres se pide:
+
+a. generar una nueva estructura que contenga para cada patente del vehiculo, el totla de kms realizados durante todo el 2023. esta informacion debe quedar ordenada por patente en forma ascendente. 
+
+b. informar la patente del vehiculo que recaudo mas dinero en alquiler durante todo el año 2023. 
+
+c. informar el porcentaje de alquileres realizados cuyos automoviles se retiran en una ciudad y se devuelven en otra ciudad 
+
+d. informar el mes en que se retiraron menos vehiculos alquilados }
