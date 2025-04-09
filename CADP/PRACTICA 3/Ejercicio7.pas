29baef86d5f0a9a3255980cbd191d98ac3817c22
Hernan <83614099+Hernan404@users.noreply.github.com>
2025-04-09 11:35:33 -0300

    Update Ejercicio7.pas

--- CADP/PRACTICA 3/Ejercicio7.pas
+++ CADP/PRACTICA 3/Ejercicio7.pas
@@ -1,10 +1,10 @@
-Realizar un programa que lea información de centros de investigación de Universidades Nacionales. De cada
+{Realizar un programa que lea información de centros de investigación de Universidades Nacionales. De cada
 centro se lee su nombre abreviado (ej. LIDI, LIFIA, LINTI), la universidad a la que pertenece, la cantidad de
 investigadores y la cantidad de becarios que poseen. La información se lee de forma consecutiva por
 universidad y la lectura finaliza al leer un centro con 0 investigadores, que no debe procesarse. Informar:
 ● Cantidad total de centros para cada universidad.
 ● Universidad con mayor cantidad de investigadores en sus centros.
-● Los dos centros con menor cantidad de becarios.
+● Los dos centros con menor cantidad de becarios.}
 
 program EJ7P3;
 type 
