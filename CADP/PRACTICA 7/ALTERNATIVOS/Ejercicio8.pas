17f609855302901e60eb1d8458026585ccd63480
Hernan <83614099+Hernan404@users.noreply.github.com>
2025-05-30 11:09:54 -0300

    Create Ejercicio8.pas

---
+++ CADP/PRACTICA 7/ALTERNATIVOS/Ejercicio8.pas
@@ -0,0 +1,25 @@
+{Una empresa de mensajería nacional solicita desarrollar un programa destinado a
+la administración de encomiendas enviadas entre distintas sucursales del país, 
+efectuadas entre los meses de Febrero y Diciembre del año 2023.
+
+La empresa dispone de una lista de encomiendas despachadas en ese período. 
+De cada encomienda se conoce: código de sucursal origen, DNI del responsable de envío, c
+ódigo de sucursal destino, DNI del responsable de recepción, fecha de envío, hora,
+peso del paquete (en kg) y 
+el código del tipo de encomienda (1: documentos, 2: mercadería, 3: repuestos, 4: electrodomésticos, 5: libros, 6: prendas de vestir, 7: otros). 
+Esta estructura no posee orden alguno.
+Se pide:
+
+a) Generar una nueva estructura que contenga solo las encomiendas entre diferentes responsables 
+(es decir, aquellas en las que los DNIs de origen y destino son distintos). 
+Esta nueva estructura debe estar ordenada por código de sucursal origen.
+
+Una vez generada la estructura del inciso a), utilizar dicha estructura para:
+
+b) Calcular e informar para cada sucursal de origen, el peso total de encomiendas enviadas a 
+otros responsables.
+
+c) Calcular e informar cuál es el tipo de encomienda que tuvo más envíos entre diferentes responsables.
+
+d) Calcular e informar la cantidad de encomiendas entre diferentes responsables realizadas 
+en el mes de Septiembre en las cuales el código de sucursal destino tenga más dígitos impares que pares.}
