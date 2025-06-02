7343316ba0b2f63d2937c71ddac3d032299bb679
Hernan <83614099+Hernan404@users.noreply.github.com>
2025-06-02 13:09:39 -0300

    Create EJercicio9.pas

---
+++ CADP/PRACTICA 7/ALTERNATIVOS/EJercicio9.pas
@@ -0,0 +1,31 @@
+{Una biblioteca municipal posee la lista de libros que fueron leídos durante el mes de Octubre. De cada libro se tiene:
+
+    código de libro,
+
+    título del libro,
+
+    código de categoría (1: novela, 2: ciencia ficción, 3: historia, 4: biografía, 5: poesía, 6: ensayo, 7: infantil, 8: misterio),
+
+    puntaje promedio otorgado por los lectores.
+
+Dicha estructura no posee orden alguno.
+
+Se pide:
+
+a) Actualizar (en la lista que se dispone) el puntaje promedio otorgado por los lectores. Para ello se debe leer desde teclado las opiniones que han realizado los socios de la biblioteca. De cada opinión se lee:
+
+    DNI del lector,
+
+    apellido y nombre del lector,
+
+    código de libro,
+
+    puntaje otorgado.
+
+La lectura finaliza cuando se lee el código de libro -1. La información viene ordenada por código de libro.
+
+b) Informar el código de categoría que obtuvo mayor puntaje total entre todas las opiniones.
+
+c) Informar el apellido y nombre de aquellos lectores que posean la misma cantidad de dígitos pares que impares en su DNI.
+
+d) Realizar un módulo que elimine de la lista que se dispone un libro cuyo código se recibe como parámetro (el mismo puede no existir).}
