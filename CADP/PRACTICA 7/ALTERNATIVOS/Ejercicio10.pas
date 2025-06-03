e9c73c087025b832bdfbba940526c2156114d24d
Hernan <83614099+Hernan404@users.noreply.github.com>
2025-06-03 13:13:49 -0300

    Create Ejercicio10.pas

---
+++ CADP/PRACTICA 7/ALTERNATIVOS/Ejercicio10.pas
@@ -0,0 +1,23 @@
+Una empresa de servicios informáticos desea procesar la información de las compañías a las que 
+les desarrolla software. De cada compañía se conoce su ID, nombre, 
+si es pública o privada, el nombre de la ciudad donde está radicada y
+los proyectos de software que tiene contratados (a lo sumo 20). 
+Para cada proyecto se registra: tipo de software (ERP, CRM, e-commerce, logística, etc.), 
+cantidad de usuarios que lo utilizan y la duración del proyecto en meses.
+
+a. Realizar un programa que lea la información de las compañías y sus proyectos.
+Dicha información se ingresa hasta que llegue una compañía con ID -1 (la cual no debe procesarse).
+Para cada compañía se leen todos sus proyectos, hasta que se ingrese un proyecto con 0 usuarios 
+(que no debe procesarse).
+
+Una vez leída y almacenada la información, calcular e informar:
+b. Nombres de las compañías radicadas en "Villa María" que tienen proyectos 
+del tipo ERP y cuyo ID posee al menos dos cifras 7.
+
+c. La cantidad total de usuarios de proyectos del tipo CRM y qué porcentaje 
+representa con respecto al total de usuarios registrados en todos los proyectos.
+
+d. La compañía que tiene el proyecto de tipo "e-commerce" con mayor duración (en meses).
+
+e. Realizar un módulo que incremente en 1 mes la duración de todos los proyectos de tipo logística que 
+tienen menos de 10 usuarios, solo en compañías privadas.
