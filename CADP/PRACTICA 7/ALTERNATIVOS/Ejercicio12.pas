045282701a9e492e80a0b5ef6bd9f85bbf9b289a
Hernan <83614099+Hernan404@users.noreply.github.com>
2025-06-06 10:34:11 -0300

    Create Ejercicio12.pas

---
+++ CADP/PRACTICA 7/ALTERNATIVOS/Ejercicio12.pas
@@ -0,0 +1,29 @@
+
+{Realizar un programa para una empresa organizadora que necesita gestionar 100 torneos de videojuegos. De cada torneo se dispone la siguiente información:
+
+    nombre del torneo,
+
+    categoría del torneo (1: FPS, 2: MOBA, 3: Lucha, 4: Deportes, 5: Estrategia),
+
+    sede del torneo,
+
+    cantidad máxima de jugadores permitidos para participar en el torneo,
+
+    y costo de inscripción por jugador.
+
+Se pide:
+a. Generar una estructura con las inscripciones para dichos torneos. De cada inscripción se debe guardar:
+
+    código de inscripción,
+
+    número de torneo (1..100),
+
+    DNI del jugador,
+
+    y cantidad de inscripciones (por si un mismo jugador inscribe a varios equipos).
+    La lectura de las inscripciones finaliza con código de inscripción -1.
+
+Una vez leída y almacenada la información de las inscripciones, calcular e informar:
+a. El nombre y sede de los dos torneos con menor recaudación.
+b. La cantidad de inscripciones en las que el DNI del jugador contiene más dígitos pares que impares, y que sean para torneos de categoría “Lucha”.
+c. Informar si la cantidad total de inscripciones del torneo número 50 alcanzó la cantidad máxima de jugadores permitidos.}
