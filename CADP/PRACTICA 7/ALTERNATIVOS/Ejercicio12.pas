8dedb2fd8a66978ca807d58a99e64f48a396eb10
Hernan <83614099+Hernan404@users.noreply.github.com>
2025-06-09 13:00:06 -0300

    Create Ejercicio12.pas

---
+++ CADP/PRACTICA 7/ALTERNATIVOS/Ejercicio12.pas
@@ -0,0 +1,18 @@
+{La academia de idiomas LangCenter desea procesar la información de los 4 planes de cursos que ofrece:
+
+    Inglés básico, 2) Conversación avanzada, 3) Preparación para exámenes, 4) Acceso total a todos los cursos.
+    Para ello, la academia dispone de una tabla con la información sobre el costo mensual de cada plan.
+
+Realizar un programa que lea y almacene la información de los alumnos inscriptos en la academia. 
+De cada alumno se conoce el nombre, DNI, edad y plan contratado (valor entre 1 y 4). Cada alumno tiene un solo plan contratado.
+La lectura finaliza cuando se lee el alumno con DNI 0, el cual no debe procesarse.
+
+Una vez almacenados todos los datos, procesar la estructura de datos generada, calcular e informar:
+
+    La ganancia total de LangCenter.
+
+    Los dos planes con mayor cantidad de inscriptos.
+
+    Genere una lista con nombre y DNI de los alumnos de más de 40 años 
+    que están inscriptos en Preparación para exámenes o Acceso total a todos los cursos. 
+    Esta lista debe estar ordenada por DNI.}
