f75a379e4ba75f5003e5806a46079075abb3cf6e
Hernan <83614099+Hernan404@users.noreply.github.com>
2025-06-16 12:36:23 -0300

    Create primer semestre.pas

---
+++ CADP/PARCIALES/2023/TERCERA FECHA/primer semestre.pas
@@ -0,0 +1,13 @@
+{
+una revista deportiva dispone de informacin de los jugadores de futbol participantes de la liga 
+profesional 2022. de cada jugador se conoce: codigo de jugador, apellido y nombre, codigo
+de equipo (1..28), año de nacimiento y la calificacion obtenida para cada una de las 27 fechas 
+del torneo finalizado. la calificacion es numerica de 0 a 10, donde el valor 0 significa que el jugador
+no ha participado en la fecha.
+
+A. informar para cada equipo la cantidad de jugadores mayores a 35 años 
+B. informar los codigos de los 2 jugadores con mejor calificacion en los partidos en los que 
+solo participo. solo debe considerarse los jugadores que participen en mas de 14 fechas.
+C. implementar e invocar a un modulo que genere una lista con los jugadores cuyo codigo posee
+exactamente 3 digitos impares y haya nacido entre 1983 y 1990. la lista debe estar ordenada por codigo de jugador
+}
